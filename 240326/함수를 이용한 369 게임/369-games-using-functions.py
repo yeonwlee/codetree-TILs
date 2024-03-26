d7494c441805e3220eff9258861a1d97935c49cf
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-26 22:35:58 +0900

    Title: 함수를 이용한 369 게임, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240326/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,23 @@
+a, b = map(int, input().split())
+
+
+#3, 6, 9 를 가지고 있는지 확인
+def has_target_num(num: int) -> bool:
+    while num > 0:
+        cur_position_num = num % 10
+        if cur_position_num == 3 or cur_position_num == 6 or cur_position_num == 9:
+            return True
+        num //= 10
+    return False
+
+
+def is_muitiple_value_of_3(num: int) -> bool:
+    return num % 3 == 0
+
+
+count: int = 0
+for cur_num in range(a, b + 1):
+    if has_target_num(cur_num) or is_muitiple_value_of_3(cur_num):
+        count += 1
+
+print(count)
