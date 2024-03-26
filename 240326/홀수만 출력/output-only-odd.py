4472c1c4c2dd1118311764f1bc7c9d79a6fa935f
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-26 13:04:13 +0900

    Title: 홀수만 출력, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/홀수만 출력/output-only-odd.py
+++ 240326/홀수만 출력/output-only-odd.py
@@ -1,5 +1,4 @@
 a, b = map(int, input().split())
 odd_numbers_in_between = list(number for number in range(a, b + 1) if number % 2 == 1)
-odd_numbers_in_between.sort()
 for odd_number in odd_numbers_in_between:
     print(odd_number, end=" ")
