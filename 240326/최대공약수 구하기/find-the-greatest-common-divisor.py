1dd176194b64cf20056581ac637b69388f87d0cc
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-26 16:03:31 +0900

    Title: 최대공약수 구하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240326/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,8 +1,15 @@
 import math
 
 
-def print_gcd(num1, num2) -> None:
-    print(math.gcd(num1, num2))
+# def print_gcd(num1, num2) -> None:
+#     print(math.gcd(num1, num2))
+
+
+def print_gcd(num1, num2):
+    for i in range(min(num1, num2), 0, -1):  # min(n, m)부터 1까지 역순으로 반복
+        if num1 % i == 0 and num2 % i == 0:
+            print(i)  
+            break
 
 
 num1, num2 = map(int, input().split())
