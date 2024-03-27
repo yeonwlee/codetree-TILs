0fefa9aae1b62c7c66af43aeb585894258114f91
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 13:54:50 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240327/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,8 +2,8 @@ def is_part_of_source(source: list[int], check_data: list[int]) -> bool:
     if len(source) < len(check_data):
         return False
     for index, _ in enumerate(source):
-        if len(source) > len(check_data) + index - 1:
-            if source[index:len(check_data) + index - 1] == check_data:
+        if len(source) >= len(check_data) + index:
+            if source[index:len(check_data) + index] == check_data:
                 return True
     return False
 
