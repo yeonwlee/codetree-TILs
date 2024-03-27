b7f93854ea5ac3aa9543f465dc86646d9bcc81cb
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 13:17:13 +0900

    Title: 함수를 이용한 온전수 판별, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 240327/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -1,5 +1,13 @@
+# def is_target_value(number: int) -> bool:
+#     return number % 2 != 0 and str(number)[-1] != "5" and (number % 3 != 0 or number % 9 == 0)
+
+
+# a, b = map(int, input().split())
+# print(sum(1 for cur_num in range(a, b + 1) if is_target_value(cur_num)))
+
+
 def is_target_value(number: int) -> bool:
-    return number % 2 != 0 and str(number)[-1] != "5" and (number % 3 != 0 or number % 9 == 0)
+    return number % 2 != 0 and number % 10 != 5 and (number % 9 == 0 or number % 3 != 0)
 
 
 a, b = map(int, input().split())
