60e9879860712aeb6e22130611cb4e2b9f3776cf
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 14:06:42 +0900

    Title: 2021년 날짜의 유무, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/2021년 날짜의 유무/with-or-without-2021.py
+++ 240327/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,9 +2,11 @@ date_month_has = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30,
 
 
 def has_it_a_date(month: int, date: int) -> bool:
-    if 1 <= date <= date_month_has[month]:
-        return True
+    if month in date_month_has:
+        if 1 <= date <= date_month_has[month]:
+            return True
     return False
+
 
 month, date = map(int, input().split())
 print("Yes" if has_it_a_date(month, date) else "No")
