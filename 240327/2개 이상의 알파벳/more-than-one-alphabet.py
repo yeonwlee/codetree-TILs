468452124b1c24e2aaa9be614213d4321986e01c
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 15:17:20 +0900

    Title: 2개 이상의 알파벳, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240327/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,5 +1,7 @@
-def check_duplication(string: list[str]) -> bool:
-    return len(string) > len(set(string))
+#문제 잘못 이해함. '서로 다른 알파벳의 수'임. '같은 알파벳의 수'가 아니라
+
+def check_it_has_different_chars(string: list[str]) -> bool:
+    return len(set(string)) >= 2
 
 string_list = list(input())
-print("Yes" if check_duplication(string_list) else "No")
+print("Yes" if check_it_has_different_chars(string_list) else "No")
