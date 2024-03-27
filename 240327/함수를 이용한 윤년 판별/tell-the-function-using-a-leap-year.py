02d6939ac5f00da6da824bba33dddd087849ec7a
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 10:31:36 +0900

    Title: 함수를 이용한 윤년 판별, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240327/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,10 +1,18 @@
-def is_yunnyun(year: int) -> str:
-    if year % 4 == 0:
-        if year % 100 == 0 and year % 400 != 0:
-            return "false"
-        return "true"
-    return "false"
+# def is_yunnyun(year: int) -> str:
+#     if year % 4 == 0:
+#         if year % 100 == 0 and year % 400 != 0:
+#             return "false"
+#         return "true"
+#     return "false"
+
+
+# year = int(input())
+# print(is_yunnyun(year))
+
+
+def is_leap_year(year: int) -> bool:
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) #100으로 나누어 떨어지되 400으로 나누어 떨어지지 않는 해는 평년
 
 
 year = int(input())
-print(is_yunnyun(year))
+print("true" if is_leap_year(year) else "false")
