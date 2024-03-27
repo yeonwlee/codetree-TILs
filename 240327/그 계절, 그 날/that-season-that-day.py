d905eff35bfbe37486cda61089cec4625b320d80
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 14:26:36 +0900

    Title: 그 계절, 그 날, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/그 계절, 그 날/that-season-that-day.py
+++ 240327/그 계절, 그 날/that-season-that-day.py
@@ -1,5 +1,4 @@
 date_in_month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-season = {(3, 4, 5): "Spring", (6, 7, 8): "Summer", (9, 10, 11): "Fall", (12, 1, 2): "Winter"}
 
 
 def is_leap_year(year: int) -> bool:
@@ -10,13 +9,23 @@ def is_valid_date(month: int, date: int) -> bool:
     return date <= date_in_month[month] #주어진 조건상 이것으로 충분
 
 
-year, month, date = map(int, input().split())
+def get_season(month: int) -> str:
+    if month in {3, 4, 5}:
+        return "Spring"
+    elif month in {6, 7, 8}:
+        return "Summer"
+    elif month in {9, 10, 11}:
+        return "Fall"
+    else:
+        return "Winter"
 
+
+year, month, date = map(int, input().split())
 
 if is_leap_year(year):
     date_in_month[2] = 29
 
 if is_valid_date(month, date):
-    print([season[target_month] for target_month in season.keys() if month in target_month][0])
+    print(get_season(month))
 else:
     print(-1)
