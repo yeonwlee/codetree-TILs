b3071bc7b1b9429e4d04c72310d34c69f040e521
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 15:46:33 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -8,7 +8,8 @@ def get_start_index_part_of_source(source: str, target: str) -> int:
 input_string = input() #기준 문자열
 target_string = input() #기준 문자열에서 부분 문자열로 존재하는지 확인할 문자열
 
-if start_index:=get_start_index_part_of_source(input_string, target_string):
+# if start_index:=get_start_index_part_of_source(input_string, target_string): # 이렇게 하면 start_index가 0인 경우 false로 인식해버림
+if (start_index:=get_start_index_part_of_source(input_string, target_string)) is not None:
     print(start_index)
 else:
     print(-1)
