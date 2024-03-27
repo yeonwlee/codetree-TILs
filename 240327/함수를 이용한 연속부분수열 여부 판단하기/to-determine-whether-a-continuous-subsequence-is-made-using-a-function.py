bd4e09608f83175c4bc6dd33b3f7919985a167cf
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 13:59:09 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240327/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,11 +1,22 @@
+# def is_part_of_source(source: list[int], check_data: list[int]) -> bool:
+#     if len(source) < len(check_data):
+#         return False
+#     for index, _ in enumerate(source):
+#         if len(source) >= len(check_data) + index:
+#             if source[index:len(check_data) + index] == check_data:
+#                 return True
+#     return False
+
+
+# n1, n2 = map(int, input().split())
+
+# arr1 = list(map(int, input().split()))
+# arr2 = list(map(int, input().split()))
+# print("Yes" if is_part_of_source(arr1, arr2) else "No")
+
+
 def is_part_of_source(source: list[int], check_data: list[int]) -> bool:
-    if len(source) < len(check_data):
-        return False
-    for index, _ in enumerate(source):
-        if len(source) >= len(check_data) + index:
-            if source[index:len(check_data) + index] == check_data:
-                return True
-    return False
+    return any(source[index:index+len(check_data)] == check_data for index in range(len(source) - len(check_data) + 1))
 
 
 n1, n2 = map(int, input().split())
