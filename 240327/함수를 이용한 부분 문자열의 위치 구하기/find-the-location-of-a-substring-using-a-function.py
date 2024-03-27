208d5c106652ca034401347312785d447e0ee165
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 16:06:45 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240327/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,7 +1,31 @@
+#슬라이싱 사용시. 슬라이싱하면서 계속 객체를 만들기때문에 메모리 사용량이 높을 수 있음
+# def get_start_index_part_of_source(source: str, target: str) -> int:
+#     for index in range(len(source) - len(target) + 1):
+#         if source[index: len(target) + index] == target:
+#             return index
+#     return None
+
+
+# input_string = input() #기준 문자열
+# target_string = input() #기준 문자열에서 부분 문자열로 존재하는지 확인할 문자열
+
+# # if start_index:=get_start_index_part_of_source(input_string, target_string): # 이렇게 하면 start_index가 0인 경우 false로 인식해버림
+# if (start_index:=get_start_index_part_of_source(input_string, target_string)) is not None:
+#     print(start_index)
+# else:
+#     print(-1)
+
+
 def get_start_index_part_of_source(source: str, target: str) -> int:
-    for index in range(len(source) - len(target) + 1):
-        if source[index: len(target) + index] == target:
-            return index
+    if len(source) < len(target): #target이 부분문자열인게 불가
+        return None
+    for base_index in range(len(source)):
+        for target_index in range(len(target)):
+            if base_index + target_index < len(source):
+                if source[base_index + target_index] != target[target_index]:
+                    break
+                if target_index == len(target) - 1:
+                    return base_index
     return None
 
 
