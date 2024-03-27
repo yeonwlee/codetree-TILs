fa739c07f98496d6b41671fea08094ba66d6e437
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 15:20:48 +0900

    Title: 2개 이상의 알파벳, Time: 139ms, Memory: 73MB, Status: Passed - CodeTree

--- 240327/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240327/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,7 +1,8 @@
 #문제 잘못 이해함. '서로 다른 알파벳의 수'임. '같은 알파벳의 수'가 아니라
 
-def check_it_has_different_chars(string: list[str]) -> bool:
+def check_it_has_different_chars(string: str) -> bool: #str도 이터러블 객체이기 때문에 바로 set으로 변환 가능 
     return len(set(string)) >= 2
 
-string_list = list(input())
+
+string_list = input()
 print("Yes" if check_it_has_different_chars(string_list) else "No")
