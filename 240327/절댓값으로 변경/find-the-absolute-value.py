8af780b4e638c1dd938ef15be8e211a0395d57f0
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 15:09:11 +0900

    Title: 절댓값으로 변경, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/절댓값으로 변경/find-the-absolute-value.py
+++ 240327/절댓값으로 변경/find-the-absolute-value.py
@@ -1,6 +1,5 @@
 n = int(input())
 
 number_arr = map(int, input().split())
-abs_number_arr = [abs(cur_num) for cur_num in number_arr]
-for abs_number in abs_number_arr:
-    print(abs_number, end=" ")
+for cur_num in number_arr:
+    print(abs(cur_num), end=" ")
