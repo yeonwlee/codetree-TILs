2a762ccc5cb074d1d1195d2457c4bd8f4b30c268
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 13:07:09 +0900

    Title: 사칙연산 함수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240327/사칙연산 함수/quadratic-operations-function.py
+++ 240327/사칙연산 함수/quadratic-operations-function.py
@@ -20,7 +20,7 @@ valid_operaters = {"+", "-", "/", "*"}
 if operator in valid_operaters:
     if operator == "+":
         plus(num1, num2)
-    if operator == "-":
+    elif operator == "-":
         minus(num1, num2)
     elif operator == "/": #조건상 num2가 0이 될 일은 없음
         divide(num1, num2)
