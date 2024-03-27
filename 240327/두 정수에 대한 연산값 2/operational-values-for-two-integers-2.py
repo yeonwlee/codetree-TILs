ba4dc8128605be99e3f9ab7b25051a4c9e3214e3
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-03-27 15:34:54 +0900

    Title: 두 정수에 대한 연산값 2, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240327/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -0,0 +1,12 @@
+def calculate(num1: int, num2: int)-> tuple[int]:
+    if num1 > num2:
+        num1 *= 2
+        num2 += 10
+    else:
+        num1 += 10
+        num2 *= 2
+    return num1, num2
+
+a, b = map(int, input().split())
+a, b = calculate(a, b)
+print(a, b)
