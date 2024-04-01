83b924d4db0f68545812ea2a760030fe92bb84b0
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-01 20:45:56 +0900

    Title: 중앙값 계산 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/중앙값 계산 2/get-median-2.py
+++ 240401/중앙값 계산 2/get-median-2.py
@@ -4,7 +4,7 @@ numbers = list(map(int, input().split()))
 cur_numbers = [numbers[0]]
 mid_numbers = [numbers[0]]
 
-for index in range(1, len(numbers) - 1,2):
+for index in range(1, len(numbers),2):
     cur_numbers.append(numbers[index])
     cur_numbers = sorted(cur_numbers)
     mid_numbers.append(cur_numbers[len(cur_numbers) // 2])
