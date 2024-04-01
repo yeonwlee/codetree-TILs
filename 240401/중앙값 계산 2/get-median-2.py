6d903bf9f49bef3ac0f368614800ec61cc558783
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-01 20:50:39 +0900

    Title: 중앙값 계산 2, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240401/중앙값 계산 2/get-median-2.py
+++ 240401/중앙값 계산 2/get-median-2.py
@@ -6,9 +6,9 @@ mid_numbers = [numbers[0]]
 
 for index in range(1, len(numbers),2):
     cur_numbers.append(numbers[index])
+    cur_numbers.append(numbers[index + 1])
     cur_numbers = sorted(cur_numbers)
     mid_numbers.append(cur_numbers[len(cur_numbers) // 2])
-    cur_numbers.append(numbers[index + 1])
 
 for mid in mid_numbers:
     print(mid,end=" ")
