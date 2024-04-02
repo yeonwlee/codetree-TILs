1843144a46e935496f3475775a64eec1a589357b
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-02 12:00:29 +0900

    Title: 국영수 순이지, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240402/국영수 순이지/korean-english-math-order.py
+++ 240402/국영수 순이지/korean-english-math-order.py
@@ -4,8 +4,7 @@ datas = [
     for _ in range(num_of_datas)
 ]
 
-datas.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True) # 국, 영, 수 우선순위. 
-#문자열은 - 연산자가 사용이 안되어 내림차순 정렬을 위해 reverse 옵션 추가
+datas.sort(key=lambda x: (-int(x[1]), -int(x[2]), -int(x[3]))) # 국, 영, 수 우선순위. 
 
 for data in datas:
     print(*data)
