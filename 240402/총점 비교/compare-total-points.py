3f5ec1aba397958485cacf1a546694e3421ecc13
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-02 12:11:01 +0900

    Title: 총점 비교, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240402/총점 비교/compare-total-points.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, subject1, subject2, subject3):
+        self.name = name
+        self.subject1 = subject1
+        self.subject2 = subject2
+        self.subject3 = subject3
+
+    def sum_points(self):
+        return sum(int(getattr(self, attr)) for attr in ['subject1', 'subject2', 'subject3'])
+
+
+num_of_students = int(input())
+students = []
+for _ in range(num_of_students):
+    student_info = input().split()
+    students.append(Student(student_info[0], student_info[1], student_info[2], student_info[3]))
+
+students.sort(key=lambda x: x.sum_points())
+
+for student in students:
+    print(*student.__dict__.values())
