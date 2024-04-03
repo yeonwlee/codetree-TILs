fb01cb3c4d5186e9a631c5121aae189ea2efb0d0
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-03 22:13:01 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/Date to Date/date-to-date.py
+++ 240403/Date to Date/date-to-date.py
@@ -1,6 +1,6 @@
 month1, date1, month2, date2 = map(int, input().split())
 
-date_in_month = [1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 1월 1일부터, 시작일 포함이므로 인덱스 0을 1로 줌
-date_to_date = (sum(date_in_month[:month2 - 1]) + date2) - (sum(date_in_month[:month1 - 1]) + date1)
+date_in_month = [1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # 1월 1일부터 날짜를 세기 때문에 인덱스 0 을 1로 둠
+date_to_date = (sum(date_in_month[:month2]) + date2 + 1) - (sum(date_in_month[:month1]) + date1)
 
-print(1 if date_to_date == 0 else date_to_date)
+print(date_to_date)
