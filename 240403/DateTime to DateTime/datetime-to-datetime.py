a671d444f69bf78fd8f791f1cfc9e00ff05fc755
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-03 22:24:44 +0900

    Title: DateTime to DateTime, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/DateTime to DateTime/datetime-to-datetime.py
+++ 240403/DateTime to DateTime/datetime-to-datetime.py
@@ -6,9 +6,10 @@ def hour_to_minute(hour: int) -> int:
     return hour * 60
 
 
-date, hour, minute = map(int, input().split())
+days, hour, minute = map(int, input().split())
 
 base_date_time_to_minute = day_to_minute(11) + hour_to_minute(11) + 11
-compare_date_time_to_minute = day_to_minute(date) + hour_to_minute(hour) + minute
+compare_date_time_to_minute = day_to_minute(days) + hour_to_minute(hour) + minute
+time_diffrence = compare_date_time_to_minute - base_date_time_to_minute
 
-print(-1 if base_date_time_to_minute > compare_date_time_to_minute else compare_date_time_to_minute - base_date_time_to_minute)
+print(-1 if base_date_time_to_minute > compare_date_time_to_minute else time_diffrence)
