2db42fc5776b1f799d50572bc1e385102f7141e9
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-03 22:05:24 +0900

    Title: Date to Date, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/Date to Date/date-to-date.py
+++ 240403/Date to Date/date-to-date.py
@@ -1,5 +1,6 @@
 month1, date1, month2, date2 = map(int, input().split())
 
 date_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+date_to_date = (sum(date_in_month[:month2 - 1]) + date2) - (sum(date_in_month[:month1 - 1]) + date1)
 
-print((sum(date_in_month[:month2 - 1]) + date2) - (sum(date_in_month[:month1 - 1]) + date1))
+print(1 if date_to_date == 0 else date_to_date)
