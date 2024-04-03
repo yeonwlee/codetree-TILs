c197a309a9ed88e46dbd055151616ae11be1816b
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-03 22:43:28 +0900

    Title: 요일 맞추기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/요일 맞추기/guess-day-of-week.py
+++ 240403/요일 맞추기/guess-day-of-week.py
@@ -3,4 +3,4 @@ days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 month1, day1, month2, day2 = map(int, input().split())
 day_difference = abs((sum(days_in_month[:month1]) + day1) - (sum(days_in_month[:month2]) + day2))
-print(days_of_week[7 % day_difference - 1])
+print(days_of_week[-(day_difference % 7)])
