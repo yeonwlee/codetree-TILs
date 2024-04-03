0b6a62b2526f0cf38d23bcda1562f40eb3687ba6
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-03 23:07:45 +0900

    Title: 그 요일은, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/그 요일은/the-day-of-the-day.py
+++ 240403/그 요일은/the-day-of-the-day.py
@@ -5,4 +5,6 @@ month1, day1, month2, day2 = map(int, input().split())
 target_day = input()
 
 day_difference = (sum(days_of_month[:month2]) + day2) - (sum(days_of_month[:month1]) + day1)
-print(day_difference // 7 + 1)
+end_date_day = days_of_week[day_difference % 7]
+is_more_target_day = days_of_week.index(target_day) <= days_of_week.index(end_date_day)
+print(day_difference // 7 + 1 if is_more_target_day else day_difference // 7)
