f627808867688868e32b98c1d3790a857435ae8a
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-04 15:35:29 +0900

    Title: 2진수로 변환하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240404/2진수로 변환하기/convert-to-binary.py
+++ 240404/2진수로 변환하기/convert-to-binary.py
@@ -1,2 +1,14 @@
 digit_num = int(input())
-print(bin(digit_num)[2:])
+#print(bin(digit_num)[2:])
+
+def make_binary_num(num: int) -> None:
+    if num < 2:
+        print(num, end="")
+        return
+    
+    div_result, remain = divmod(num, 2)
+    make_binary_num(div_result)
+    print(remain, end="")
+    
+
+make_binary_num(digit_num)
