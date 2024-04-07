d560e1db9230397603f091e47c0a03008500a1b2
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-08 01:01:56 +0900

    Title: 왔다 갔던 구역 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240408/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,22 @@
+num_of_command = int(input())
+point_info = [0 for _ in range(101)]
+
+commands = [
+    tuple(input().split())
+    for _ in range(num_of_command)
+]
+
+cur_position = 0
+
+for x, direction in commands:
+    x = int(x)
+    if direction == "L":
+        for index in range(cur_position, cur_position - x, -1):
+            point_info[index] += 1
+        cur_position -= x
+    else: 
+        for index in range(cur_position, cur_position + x):
+            point_info[index] += 1
+        cur_position += x
+
+print(sum(1 for point in point_info if point >= 2 ))
