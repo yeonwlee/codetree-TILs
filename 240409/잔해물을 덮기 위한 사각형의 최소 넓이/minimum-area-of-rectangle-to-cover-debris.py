0d856485e166cc85a0359bf3810f24e7f04fd4c7
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-09 12:52:31 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 151ms, Memory: 54MB, Status: Wrong Answer - CodeTree

--- 240409/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240409/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -28,6 +28,8 @@ for row in range(x1, x2):
             remained_x.append(row)
             remained_y.append(col)
 
-minx, maxx = remained_x[0], remained_x[-1] + 1 #1을 더해줘야 선분 길이가 나옴
-miny, maxy = remained_y[0], remained_y[-1] + 1
+minx, maxx, miny, maxy = x1, x2, y1, y2
+if remained_x and remained_y:
+    minx, maxx = remained_x[0], remained_x[-1] + 1 #1을 더해줘야 선분 길이가 나옴
+    miny, maxy = remained_y[0], remained_y[-1] + 1
 print((maxx - minx) * (maxy - miny))
