40d03fb4d0dcb19206540a6ccc2659a2b1f09169
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-10 22:57:02 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 142ms, Memory: 54MB, Status: Wrong Answer - CodeTree

---
+++ 240410/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,35 @@
+OFFSET = 1000
+
+def mark_rect_area(area: list, rect: tuple, mark: int) -> None:
+    x1, y1, x2, y2 = list(map(lambda x: x + OFFSET, rect))
+    for row in range(x1, x2):
+        for col in range(y1, y2):
+            area[row][col] = mark
+
+
+rect1 = tuple(map(int, input().split()))
+rect2 = tuple(map(int, input().split()))
+
+area = [
+    [0 for _ in range(2000)] for _ in range(2000)
+]
+
+mark_rect_area(area, rect1, 1)
+mark_rect_area(area, rect2, 0)
+
+x1, y1, x2, y2 = list(map(lambda x: x + OFFSET, rect1))
+marked_x = []
+marked_y = []
+for row in range(x1, x2):
+    for col in range(y1, y2):
+        if area[row][col]:
+            marked_x.append(row)
+            marked_y.append(col)
+
+minx, maxx = x1, x2
+miny, maxy = y1, y2
+if marked_x:
+    minx, maxx = marked_x[0], marked_x[-1] + 1
+    miny, maxy = marked_y[0], marked_y[-1] + 1
+
+print((maxx - minx) * (maxy - miny))
