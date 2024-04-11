786788caa6e3503da39eb0d686ac51ea8667547e
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-11 12:19:49 +0900

    Title: 연속되는 수 4, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/연속되는 수 4/continuous-number4.py
+++ 240411/연속되는 수 4/continuous-number4.py
@@ -1,22 +1,14 @@
 num_of_numbers = int(input())
-# numbers = [
-#     int(input())
-#     for _ in range(num_of_numbers)
-# ]
-
-numbers = [int(input())]
-longest_continued_asc_count = cur_continued_asc_count = 1
+prev_number = int(input())
+longest_streak = current_streak = 1
 
 for _ in range(1, num_of_numbers):
-    numbers.append(int(input()))
-    if numbers[-2] < numbers[-1]:
-        cur_continued_asc_count += 1
-    else: #연속적으로 커지지 않은 경우
-        if longest_continued_asc_count < cur_continued_asc_count:
-            longest_continued_asc_count = cur_continued_asc_count
-        cur_continued_asc_count = 1
+    current_number = int(input())
+    if prev_number < current_number:
+        current_streak += 1
+        longest_streak = max(longest_streak, current_streak)
+    else:
+        current_streak = 1
+    prev_number = current_number
 
-if longest_continued_asc_count < cur_continued_asc_count:
-    longest_continued_asc_count = cur_continued_asc_count
-     
-print(longest_continued_asc_count)
+print(longest_streak)
