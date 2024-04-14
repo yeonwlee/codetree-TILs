1168d647c72afa35387a3902bef3e25d18521cd4
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-15 01:18:42 +0900

    Title: 수열 합치기, Time: 233ms, Memory: 38MB, Status: Passed - CodeTree

--- 240415/수열 합치기/concatenation-of-sequences.py
+++ 240415/수열 합치기/concatenation-of-sequences.py
@@ -1,6 +1,9 @@
+import heapq
+
 a_arr_size, b_arr_size = map(int, input().split())
 
-combined_arr = list(map(int, input().split())) + list(map(int, input().split()))
-combined_arr.sort()
-for num in combined_arr:
-    print(num, end=" ")
+sorted_a_arr = sorted(list(map(int, input().split())))
+sorted_b_arr = sorted(list(map(int, input().split())))
+
+combined_arr = list(heapq.merge(sorted_a_arr, sorted_b_arr))
+print(" ".join(map(str, combined_arr)))
