13527e89b499db3460fee52b3c176efd25932c06
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-14 21:48:35 +0900

    Title: 만나는 그 순간, Time: 156ms, Memory: 26MB, Status: Passed - CodeTree

--- 240414/만나는 그 순간/the-moment-we-meet.py
+++ 240414/만나는 그 순간/the-moment-we-meet.py
@@ -1,5 +1,3 @@
-a_position = [0]
-b_position = [0]
 step = 1
 
 def check_they_are_same_position() -> int:
@@ -7,26 +5,17 @@ def check_they_are_same_position() -> int:
         if index != 0 and a == b:
             return index
     return -1
-    
-def mark_position(command: tuple, mark_flag: str) -> None:
-    global a_position
-    global b_position
 
-    cur_marking_target = None
-    if mark_flag == "a":
-        cur_marking_target = a_position
-    else:
-        cur_marking_target = b_position
 
+def mark_position(cur_marking_target: list[int], command: tuple) -> None:
     direction, moving_seconds = command
     moving_seconds = int(moving_seconds)
-
-    while moving_seconds:
+    for _ in range(moving_seconds):
         if direction == "L":
             cur_marking_target.append(cur_marking_target[-1] - step)
         else:
             cur_marking_target.append(cur_marking_target[-1] + step)
-        moving_seconds -= 1    
+
 
 a_num_of_commands, b_num_of_commands = map(int, input().split())
 
@@ -40,24 +29,22 @@ b_commands = [
     for _ in range(b_num_of_commands)
 ]
 
+a_position = [0]
+b_position = [0]
 a_index = b_index = 0
 
-# while a_index < len(a_commands) and b_index < len(b_commands):
-#     mark_position(a_commands[a_index], "a")
-#     a_index += 1
-#     mark_position(b_commands[b_index], "b")
-#     b_index += 1
+max_length = max(len(a_commands), len(b_commands))
+for _ in range(max_length):
+    if a_index < len(a_commands):
+        mark_position(a_position, a_commands[a_index])
+        a_index += 1
+    if b_index < len(b_commands):
+        mark_position(b_position, b_commands[b_index])
+        b_index += 1
 
-#     if (same_position:= check_they_are_same_position()) is not None:
-#         print(same_position)
-#         break
+    if (same_position:= check_they_are_same_position()) != -1:
+        print(same_position)
+        break
 
-while a_index < len(a_commands):
-    mark_position(a_commands[a_index], "a")
-    a_index += 1
-
-while b_index < len(b_commands):
-    mark_position(b_commands[b_index], "b")
-    b_index += 1
-
-print(check_they_are_same_position())
+if same_position == -1:
+    print(same_position)
