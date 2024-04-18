5f557d6cb48ccc24f73250a65de56f7583f10599
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-18 12:14:18 +0900

    Title: 문자에 따른 명령 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240418/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,19 @@
+commands = input()
+
+def get_next_direction(cur_direction: int, next_direction: int) -> int:
+    return (cur_direction + next_direction + 4) % 4
+
+
+#북, 동, 남, 서 - 시계방향
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+cur_x, cur_y = 0, 0
+cur_direction_index = 0
+for command in commands:
+    if command in {"L", "R"}:
+        next_direction = -1 if "L" else 1
+        cur_direction_index = get_next_direction(cur_direction_index, next_direction)
+    else:
+        cur_x += dx[cur_direction_index]
+        cur_y += dy[cur_direction_index]
+
+print(cur_x, cur_y)
