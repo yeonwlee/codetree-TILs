50cad3342433e8418eb8c7f81b9c18157727aa29
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-18 17:29:50 +0900

    Title: 되돌아오기 2, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240418/되돌아오기 2/come-back-2.py
+++ 240418/되돌아오기 2/come-back-2.py
@@ -1,22 +1,24 @@
 def change_direction(cur_direction_index: int, next_direction: int) -> int:
     return (cur_direction_index + next_direction + 4) % 4
 
+
+def find_time_return_to_zero(commands: str) -> int:
+    #북, 동, 남, 서
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    cur_x, cur_y = 0, 0
+    cur_direction_index = 0 #북쪽
+    time = 0
+    for command in commands:
+        time += 1
+        if command in {"L", "R"}:
+            next_direction = -1 if command == "L" else 1 
+            cur_direction_index = change_direction(cur_direction_index, next_direction)
+        else:
+            cur_x, cur_y = cur_x + dxs[cur_direction_index], cur_y + dys[cur_direction_index]
+            if cur_x == 0 and cur_y == 0:
+                return time
+    return -1
+
+
 commands = input()
-
-#북, 동, 남, 서
-dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-cur_x, cur_y = 0, 0
-cur_direction_index = 0 #북쪽
-time = 0
-
-for command in commands:
-    time += 1
-    if command in {"L", "R"}:
-        next_direction = -1 if command == "L" else 1 
-        cur_direction_index = change_direction(cur_direction_index, next_direction)
-    else:
-        cur_x, cur_y = cur_x + dxs[cur_direction_index], cur_y + dys[cur_direction_index]
-        if cur_x == 0 and cur_y == 0:
-            break
-
-print(time if time else -1)
+print(find_time_return_to_zero(commands))
