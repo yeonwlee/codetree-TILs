2e76ddce9cfb2b00a52a0b564aa306eab8f9c3c6
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-19 11:56:14 +0900

    Title: n개의 숫자 중 최소, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/n개의 숫자 중 최소/min-of-n-num.py
+++ 240419/n개의 숫자 중 최소/min-of-n-num.py
@@ -1,6 +1,19 @@
 num_of_numbers = int(input())
 numbers = list(map(int, input().split()))
 
-min_value = min(numbers)
-count = sum(1 for num in numbers if num == min_value) 
+# min_value = min(numbers)
+# count = sum(1 for num in numbers if num == min_value) 
+# print(min_value, count)
+
+
+min_value = float('inf')
+count = 0
+
+for num in numbers:
+    if num < min_value:
+        min_value = num
+        count = 1
+    elif num == min_value:
+        count += 1
+
 print(min_value, count)
