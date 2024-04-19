48e6967fa28af148bb3b15bdb2f31bc2969619a0
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-19 10:20:25 +0900

    Title: 가장 큰 수 고르기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/가장 큰 수 고르기/picking-biggest-number.py
+++ 240419/가장 큰 수 고르기/picking-biggest-number.py
@@ -1,3 +1,9 @@
 numbers = list(map(int, input().split()))
 
-print(max(numbers))
+# print(max(numbers))
+
+max_num = float('-inf') #문제 조건상으로는 0으로 그냥 초기화해줘도 됨
+for number in numbers:
+    if number > max_num:
+        max_num = number
+print(max_num)
