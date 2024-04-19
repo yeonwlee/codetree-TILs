28dd36a440c4c5b0489f253bcba4061056632ef3
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-19 12:44:42 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/괄호 쌍 만들어주기 3/pair-parentheses-3.py
+++ 240419/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -1,10 +1,20 @@
 source = input()
-count_cases_of_valid_bracket = 0
 
-for start_bracket_index, start_bracket in enumerate(source):
-    if start_bracket == "(":
-        for _, end_bracket in enumerate(source[start_bracket_index + 1:]):
-            if end_bracket == ")":
-                count_cases_of_valid_bracket += 1
+# count_cases_of_valid_bracket = 0
+# for start_bracket_index, start_bracket in enumerate(source):
+#     if start_bracket == "(":
+#         for _, end_bracket in enumerate(source[start_bracket_index + 1:]):
+#             if end_bracket == ")":
+#                 count_cases_of_valid_bracket += 1
 
-print(count_cases_of_valid_bracket)
+# print(count_cases_of_valid_bracket)
+        
+left_bracket_count = 0
+valid_pairs = 0
+for char in source:
+    if char == '(':
+        left_bracket_count += 1
+    elif char == ')' and left_bracket_count > 0:
+        valid_pairs += left_bracket_count
+
+print(valid_pairs)
