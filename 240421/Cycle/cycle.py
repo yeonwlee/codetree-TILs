a1543900b4558339063951e5b979264e8c3a2ce8
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-21 01:11:10 +0900

    Title: Cycle, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/Cycle/cycle.py
+++ 240421/Cycle/cycle.py
@@ -1,10 +1,20 @@
 multiply_value, mod_value = map(int, input().split())
 
 
+# cur_value = multiply_value
+# results = []
+
+# while (cur_value:=cur_value * multiply_value % mod_value) not in results:
+#     results.append(cur_value)
+   
+# print(len(results[results.index(cur_value):]))
+
 cur_value = multiply_value
+set_for_checking_cycle = set()
 results = []
 
-while (cur_value:=cur_value * multiply_value % mod_value) not in results:
+while (cur_value:=cur_value * multiply_value % mod_value) not in set_for_checking_cycle:
     results.append(cur_value)
+    set_for_checking_cycle.add(cur_value)
    
 print(len(results[results.index(cur_value):]))
