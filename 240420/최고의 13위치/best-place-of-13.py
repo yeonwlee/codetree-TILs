dbe7a2d09d3593d9001aa17831fa34f7ccfe32c3
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-20 14:57:45 +0900

    Title: 최고의 13위치, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/최고의 13위치/best-place-of-13.py
+++ 240420/최고의 13위치/best-place-of-13.py
@@ -4,7 +4,7 @@ max_num_of_coins_in_area = 0
 for _ in range(width_of_grid):
     cur_row_coins = list(map(int, input().split()))
     for first_index_of_area in range(len(cur_row_coins) - 2):
-        cur_coins_in_area = sum(1 for position in cur_row_coins[first_index_of_area: first_index_of_area + 3] if position == 1)
+        cur_coins_in_area = cur_row_coins[first_index_of_area] + cur_row_coins[first_index_of_area + 1] + cur_row_coins[first_index_of_area + 2]
         max_num_of_coins_in_area = max(max_num_of_coins_in_area, cur_coins_in_area)
 
 print(max_num_of_coins_in_area)
