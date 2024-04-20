c86069f1cf826377c63e48d152e95614bb65bc2c
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-20 14:24:51 +0900

    Title: 일렬로 서있는 소 2, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240420/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,18 @@
+## 서 있는 위치도 오름차, 키도 오름차인 
+#서로 다른 쌍 구하기
+
+#소의 위치 오름차인 경우를 살피면서, 그 소의 키들을 곧바로 비교해보기
+#서로 다른 쌍을 구해야하기
+num_of_cow = int(input())
+
+cow_height_info = list(map(int, input().split()))
+asc_cow = set()
+
+for first_cow_index in range(len(cow_height_info)):
+    for second_cow_index in range(first_cow_index + 1, len(cow_height_info)):
+        for third_cow_index in range(second_cow_index + 1, len(cow_height_info)):
+            if cow_height_info[first_cow_index] <= cow_height_info[second_cow_index] <= cow_height_info[third_cow_index]:
+                asc_cow.add((cow_height_info[first_cow_index], cow_height_info[second_cow_index], cow_height_info[third_cow_index]))
+
+
+print(len(asc_cow))
