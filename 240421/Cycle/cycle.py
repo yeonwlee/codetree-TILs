3e37c497270b57023190f3b4ec97081b50bf2ac6
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-04-21 01:02:06 +0900

    Title: Cycle, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240421/Cycle/cycle.py
+++ 240421/Cycle/cycle.py
@@ -3,21 +3,8 @@ multiply_value, mod_value = map(int, input().split())
 
 cur_value = multiply_value
 results = set()
-cycle = 0
 
-# while (cur_value:=cur_value * multiply_value % mod_value) not in results:
-#     results.add(cur_value)
-#     cycle += 1
-
-# print(cycle)
-
-
-while True:
-    cur_value = cur_value * multiply_value % mod_value
-    if cur_value not in results:
-        results.add(cur_value)
-        cycle += 1
-    else:
-        break
-
-print(cycle)
+while (cur_value:=cur_value * multiply_value % mod_value) not in results:
+    results.add(cur_value)
+   
+print(len(results))
