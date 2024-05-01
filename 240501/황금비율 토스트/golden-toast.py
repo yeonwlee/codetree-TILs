fd07b242789d9352f5cca59949ad60fd42010a24
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-01 18:01:02 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/황금비율 토스트/golden-toast.py
+++ 240501/황금비율 토스트/golden-toast.py
@@ -1,119 +1,68 @@
+## TODO: 처음부터 다시
 class Node:
-    def __init__(self, data: str) -> None:
+    def __init__(self, data=None):
         self.data = data
-        self.next = None
         self.prev = None
-
+        self.next = None
 
 class DoublyLinkedList:
-    def __init__(self) -> None:
-        self.head = None
-        self.tail = None
-        self.count = 0
-        self.cur_position = None
+    def __init__(self):
+        self.head = Node()  # 시작 더미 노드
+        self.tail = Node()  # 끝 더미 노드
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def push_front(self, data: str) -> Node:
+    def insert_after(self, node, data):
         new_node = Node(data)
-        new_node.prev = None
-        if self.get_count():
-            new_node.next = self.head
-            self.head.prev = new_node
-        else:
-            new_node.next = None
-        self.head = new_node
-        self.count += 1
+        new_node.prev = node
+        new_node.next = node.next
+        node.next.prev = new_node
+        node.next = new_node
+        return new_node
 
-        return self.head
+    def delete_node(self, node):
+        if node == self.tail:
+            return None  # 타일 노드는 삭제할 수 없습니다.
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        return node.next  # 삭제된 노드의 다음 노드를 반환
 
-    def push_back(self, data: str) -> Node:
-        new_node = Node(data)
-        new_node.next = None
-        if self.get_count():
-            new_node.prev = self.tail
-            self.tail.next = new_node
-        else:
-            self.head = new_node
-            new_node.prev = None
-        self.tail = new_node
-        self.count += 1
+    def to_string(self):
+        result = []
+        current = self.head.next
+        while current != self.tail:
+            result.append(current.data)
+            current = current.next
+        return ''.join(result)
 
-        return self.tail
+def process_commands(initial_breads, commands):
+    dll = DoublyLinkedList()
+    current = dll.head
 
-    def del_node(self, node: Node) -> None:
-        if node == self.begin():  # 지우는게 head일 경우
-            self.head = node.next
-            self.head.prev = None
+    for char in initial_breads:
+        current = dll.insert_after(current, char)
 
-            node.next = None
-        elif node == self.end():  # 지우는게 tail일 경우
-            self.tail = node.prev
-            self.tail.next = None
+    for command in commands:
+        if command == 'L':
+            if current != dll.head:
+                current = current.prev
+        elif command == 'R':
+            if current.next != dll.tail:
+                current = current.next
+        elif command.startswith('P '):
+            char = command.split()[-1]
+            current = dll.insert_after(current, char)
+        elif command == 'D':
+            if current.next != dll.tail:
+                current = dll.delete_node(current.next)  # 삭제 후 다음 노드로 이동
 
-            node.prev = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-            node.prev = None
-            node.next = None
+    return dll.to_string()
 
-        self.count -= 1
+# 입력 부분 처리
+num_of_bread, num_of_commands = map(int, input().split())
+bread_info = input()
+commands = [input().strip() for _ in range(num_of_commands)]
 
-
-    def add_node(self, node: Node, data) -> Node:  # 주어진 노드의 위치에 넣음
-        next_position: Node = node.next
-        if node == self.begin():
-            next_position = self.push_front(data)
-        elif node == self.end():
-            next_position = self.push_back(data)
-        else:
-            new_node = Node(data)
-            new_node.prev = node
-            new_node.next = node.next
-            node.next.prev = new_node
-            node.next = new_node
-            self.count += 1
-
-        return next_position
-
-    def get_count(self):
-        return self.count
-
-    def begin(self):
-        return self.head
-
-    def end(self):
-        return self.tail
-
-
-num_of_bread, num_of_command = map(int, input().split())
-
-status_of_bread = list(input())
-
-# 연결리스트 세팅
-doubly_linked_list = DoublyLinkedList()
-
-for bread_info in status_of_bread:
-    doubly_linked_list.push_back(bread_info)
-
-
-# 이터레이터 세팅
-iterator = doubly_linked_list.end()
-
-for _ in range(num_of_command):
-    secret_command = input().split()
-    if secret_command[0] == 'L':  # 바로 앞에 있는 빵을 건너뛴 위치로 변경
-        if iterator.prev is not None:
-            iterator = iterator.prev
-    elif secret_command[0] == 'R':  # 바로 뒤에 있는 빵을 건너뛴 위치로 변경
-        if iterator.next is not None:
-            iterator = iterator.next
-    elif secret_command[0] == 'D':  # 바로 뒤에 있는 빵 제거
-            doubly_linked_list.del_node(iterator)
-            
-    elif secret_command[0] == 'P':  # 가리키는 위치에 식빵 추가
-        iterator = doubly_linked_list.add_node(iterator, secret_command[1])
-
-iterator = doubly_linked_list.begin()
-while iterator != None:
-    print(iterator.data, end="")
-    iterator = iterator.next
+# 결과 출력
+result = process_commands(bread_info, commands)
+print(result)
