42a1bacbdc4eb8002a477bedba2495c7651f6c2f
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-01 12:48:02 +0900

    Title: 이상한 진수 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/이상한 진수 2/awkward-digits-2.py
+++ 240501/이상한 진수 2/awkward-digits-2.py
@@ -2,19 +2,39 @@
 #가능한 숫자 N 중 최댓값을 찾는 프로그램을 작성해보세요.
 
 # 가장 높은 자리의 수 중 0 -> 1로 변경한 후 해당 수를 10진수로 다시 변경하면 되지 않을지
+# source = list(input()) # 2진수
 
-source = list(input()) # 2진수
-
-is_changed = False
-for index, position_value in enumerate(source):
-    if position_value == '0':
-        source[index] = '1'
-        is_changed = True
-        break
-
-
-if not is_changed: #아무것도 변경되지 않았다는 것은 모든 자리가 1이었음. 제일 낮은 자리수를 0으로 변경
-    source[-1] = '0'
+# is_changed = False
+# for index, position_value in enumerate(source):
+#     if position_value == '0':
+#         source[index] = '1'
+#         is_changed = True
+#         break
 
 
-print(int(''.join(source), 2))
+# if not is_changed: #아무것도 변경되지 않았다는 것은 모든 자리가 1이었음. 제일 낮은 자리수를 0으로 변경
+#     source[-1] = '0'
+
+
+# print(int(''.join(source), 2))
+
+
+####완전탐색 연습하기
+
+def change_cur_position_value(num: int) -> int:
+    return 1 - num # 1->0 , 0->1
+
+
+source = list(map(int, list(input()))) # 2진수
+maximum_value = float('-inf')
+
+for index, value in enumerate(source):
+    source[index] = change_cur_position_value(source[index])
+
+    cur_value = int(''.join(map(str, source)), 2)
+    if cur_value > maximum_value:
+        maximum_value = cur_value
+    
+    source[index] = change_cur_position_value(source[index])
+
+print(maximum_value)
