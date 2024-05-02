4c155b9d2a09ee9e4cb8c33f24bb86e70ab75e6b
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-02 15:49:31 +0900

    Title: 선택 정렬 구현, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/선택 정렬 구현/implement-selection-sort.py
+++ 240502/선택 정렬 구현/implement-selection-sort.py
@@ -4,12 +4,10 @@
 num_of_numbers = int(input())
 numbers = list(map(int, input().split()))
 
-for index in range(len(numbers)):
-    min_value = float('inf')
-    min_index = None
-    for cur_comparing_index in range(index, len(numbers)):
-        if min_value > numbers[cur_comparing_index]:
-            min_value = numbers[cur_comparing_index]
+for index in range(len(numbers) - 1): #마지막 요소는 확인할 필요가 없음
+    min_index = index
+    for cur_comparing_index in range(index + 1, len(numbers)):
+        if numbers[min_index] > numbers[cur_comparing_index]:
             min_index = cur_comparing_index
     numbers[index], numbers[min_index] = numbers[min_index], numbers[index]
 
