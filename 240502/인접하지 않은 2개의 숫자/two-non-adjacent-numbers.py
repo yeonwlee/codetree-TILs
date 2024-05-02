4d23f0278b39d4df750e309d017438e91339f424
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-02 17:24:13 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,10 @@
+num_of_numbers = int(input())
+numbers = list(map(int, input().split()))
+
+max_sum = float('-inf')
+
+for first_num_index in range(len(numbers) - 2): # 인접하지 않은 수끼리 더해야하므로, 뒤에서 3번째 인덱스까지만 돌면 됨
+    for second_num in numbers[first_num_index + 1:len(numbers)]:
+        max_sum = max(max_sum, numbers[first_num_index] + second_num)
+
+print(max_sum)
