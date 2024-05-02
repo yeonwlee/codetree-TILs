1ec547d703bcd02719340d9c46d36900f6ad038e
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-02 12:02:28 +0900

    Title: 거품 정렬 구현, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240502/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,14 @@
+num_of_numbers = int(input())
+numbers = list(map(int, input().split()))
+
+while True:
+    is_sorted = True
+    for index in range(len(numbers) - 1):
+        if numbers[index] > numbers[index + 1]:
+            numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index] 
+            is_sorted = False
+    if is_sorted:
+        break
+
+for num in numbers:
+    print(num, end=" ")
