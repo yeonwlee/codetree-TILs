ec4d0b2778e24d8aa066b625812f9d604b37e540
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-02 17:19:03 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240502/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,10 +2,22 @@ source = input()
 
 count_of_making_answer = 0
 
-for start_index in range(0, len(source)- 2):
-    if source[start_index: start_index + 2] == "((":
-        for end_index in range(start_index + 2, len(source) - 1):
-            if source[end_index: end_index + 2] == "))":
-                count_of_making_answer += 1
+# for start_index in range(0, len(source)- 2):
+#     if source[start_index: start_index + 2] == "((":
+#         for end_index in range(start_index + 2, len(source) - 1):
+#             if source[end_index: end_index + 2] == "))":
+#                 count_of_making_answer += 1
+
+# print(count_of_making_answer)
+
+
+
+## 수를 누적해서 확인
+open_count = 0
+for i in range(len(source) - 1):
+    if source[i:i+2] == '((':
+        open_count += 1
+    if source[i:i+2] == '))' and open_count > 0:
+        count_of_making_answer += open_count
 
 print(count_of_making_answer)
