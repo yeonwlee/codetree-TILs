e5847b1a804241175625363293a7896204cd6a8c
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-02 17:26:25 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240502/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -4,7 +4,7 @@ numbers = list(map(int, input().split()))
 max_sum = float('-inf')
 
 for first_num_index in range(len(numbers) - 2): # 인접하지 않은 수끼리 더해야하므로, 뒤에서 3번째 인덱스까지만 돌면 됨
-    for second_num in numbers[first_num_index + 1:]:
+    for second_num in numbers[first_num_index + 2:]:
         max_sum = max(max_sum, numbers[first_num_index] + second_num)
 
 print(max_sum)
