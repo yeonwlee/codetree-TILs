d5774236fdfb9431fa61d21eeecdd3c950f30b1c
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-03 13:06:01 +0900

    Title: 185부터 n의 10배까지, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/185부터 n의 10배까지/from-185-to-10-times-n.py
@@ -0,0 +1,7 @@
+start_num = 185
+target_num = int(input()) * 10
+
+count_of_numbers = target_num - start_num + 1
+sum_values = (start_num + target_num) * count_of_numbers / 2 #가우스 덧셈
+
+print(f"{int(sum_values)}{round(sum_values / count_of_numbers, 1): .1f}")
