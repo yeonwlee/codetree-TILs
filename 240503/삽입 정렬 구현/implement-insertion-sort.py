c10a8c7a523a8f5ba6ad8c021fd9679663231873
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-03 12:45:33 +0900

    Title: 삽입 정렬 구현, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/삽입 정렬 구현/implement-insertion-sort.py
+++ 240503/삽입 정렬 구현/implement-insertion-sort.py
@@ -1,4 +1,4 @@
-class SelectionSort():
+class InsertionSort():
     def sort_asc(self, arr: list[int]) -> None:
         for index in range(1, len(arr)):
             compare_index = index - 1 # 현재 요소와 비교할 요소의 인덱스
@@ -12,7 +12,7 @@ class SelectionSort():
 
 num_of_elements = int(input())
 numbers = list(map(int, input().split()))
-selection_sort = SelectionSort()
-selection_sort.sort_asc(numbers) # 매개변수로 넘긴 배열을 바로 변경
+insertion_sort = InsertionSort()
+insertion_sort.sort_asc(numbers) # 매개변수로 넘긴 배열을 바로 변경
 
 print(' '.join(map(str, numbers)))
