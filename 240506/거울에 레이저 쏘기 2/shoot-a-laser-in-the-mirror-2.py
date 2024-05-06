43f46ac895a3ce570d80e0d61c3e9dbdbe69ec37
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-06 17:48:16 +0900

    Title: 거울에 레이저 쏘기 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240506/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240506/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -10,29 +10,25 @@ positions =[]
 
 #########
 #시작 위치 및 방향 세팅    
-for setting_index in range(width_of_grid * 4 // 2):
-    if setting_index < width_of_grid: # 윗면
-        positions.append((0, setting_index, "D"))
-    elif setting_index < width_of_grid * 2: # 오른쪽 면
-        positions.append((setting_index - width_of_grid, width_of_grid -1, "L"))
-
-right_side = positions[width_of_grid * 2 - 1:width_of_grid - 1:-1]
-up_side = positions[width_of_grid- 1::-1]
-
+# 윗면
+for i in range(width_of_grid):
+    positions.append((0, i, "D"))
+# 오른쪽 면
+for i in range(width_of_grid):
+    positions.append((i, width_of_grid - 1, "L"))
 # 아랫쪽 면
-for cur_pos_at_side in right_side:
-    positions.append((cur_pos_at_side[1], cur_pos_at_side[0], "U"))
-
+for i in range(width_of_grid):
+    positions.append((width_of_grid - 1, width_of_grid - 1 - i, "U"))
 # 왼쪽 면
-for cur_pos_at_side in up_side:
-    positions.append((cur_pos_at_side[1], cur_pos_at_side[0], "R"))
+for i in range(width_of_grid):
+    positions.append((width_of_grid - 1 - i, 0, "R"))
 
 ########
 # /
 direction_1 = {"U": (0, 1, "R"), "D": (0, -1, "L"), "L":(-1, 0, "U"),"R":(1, 0, "D")}
 
 # \
-direction_2 = {"U": (-1, -1, "L"), "D": (0, 1, "R"), "L":(1, 0, "D"),"R":(-1, 0, "U")}
+direction_2 = {"U": (0, -1, "L"), "D": (0, 1, "R"), "L":(1, 0, "D"),"R":(-1, 0, "U")}
 
 ########
 
