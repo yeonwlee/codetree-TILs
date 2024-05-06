da38e76bafd4fd9fdd41d0ab7f849e7fce2ff817
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-06 19:03:06 +0900

    Title: 거울에 레이저 쏘기 2, Time: 218ms, Memory: 49MB, Status: Passed - CodeTree

--- 240506/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240506/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -12,37 +12,37 @@ positions =[]
 #시작 위치 및 방향 세팅    
 # 윗면
 for i in range(width_of_grid):
-    positions.append((0, i, "D"))
+    positions.append((0, i, "U"))
 # 오른쪽 면
 for i in range(width_of_grid):
-    positions.append((i, width_of_grid - 1, "L"))
+    positions.append((i, width_of_grid - 1, "R"))
 # 아랫쪽 면
 for i in range(width_of_grid):
-    positions.append((width_of_grid - 1, width_of_grid - 1 - i, "U"))
+    positions.append((width_of_grid - 1, width_of_grid - 1 - i, "D"))
 # 왼쪽 면
 for i in range(width_of_grid):
-    positions.append((width_of_grid - 1 - i, 0, "R"))
+    positions.append((width_of_grid - 1 - i, 0, "L"))
 
 ########
 # /
 direction_1 = {
-    "U": (0, 1, "R"), 
-    "D": (0, -1, "L"), 
-    "L": (-1, 0, "U"), 
-    "R": (1, 0, "D")
+    "U": (0, -1, "R"), 
+    "D": (0, 1, "L"), 
+    "L": (-1, 0, "D"), 
+    "R": (1, 0, "U")
 }
 
 # \
 direction_2 = {
-    "U": (0, -1, "L"), 
-    "D": (0, 1, "R"), 
-    "L": (1, 0, "D"), 
-    "R": (-1, 0, "U")
+    "U": (0, 1, "L"), 
+    "D": (0, -1, "R"), 
+    "L": (1, 0, "U"), 
+    "R": (-1, 0, "D")
 }
 
 
 cur_row, cur_col, direction = positions[cur_position - 1]
-count = 1
+count = 0
 
 while 0 <= cur_row < width_of_grid and 0 <= cur_col < width_of_grid:
     if mirror_grid[cur_row][cur_col] == "/":
