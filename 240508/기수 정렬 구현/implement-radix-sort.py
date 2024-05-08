6c652da9da841ef0b76c4da51ecc2d9db860ba6e
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-08 13:03:50 +0900

    Title: 기수 정렬 구현, Time: 171ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240508/기수 정렬 구현/implement-radix-sort.py
+++ 240508/기수 정렬 구현/implement-radix-sort.py
@@ -3,8 +3,8 @@ def radix_sort(arr, position) -> None:
     while position >= 0:
         buckets = [[] for _ in range(10)]
         for num_str in arr:
-            if len(num_str) > position:
-                buckets[int(num_str[position])].append(num_str)
+            digit = num_str[position] if position < len(num_str) else '0'  # 자릿수가 부족할 경우 0으로 간주
+            buckets[int(digit)].append(num_str)
         arr = [num for pos in buckets for num in pos] # 1차원화
         position -= 1
     return arr
