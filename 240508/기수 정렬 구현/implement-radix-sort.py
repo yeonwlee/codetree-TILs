3fa61821031405a10f6c2a72ce3376444f4c7c50
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-08 12:55:50 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240508/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,19 @@
+# 기수 정렬, 오름차순
+def radix_sort(arr, position) -> None:
+    buckets = [[] for _ in range(10)]
+    while position >= 0:
+        for num_str in arr:
+            if len(num_str) > position:
+                buckets[int(num_str)].append(num_str[position])
+        arr = [num for pos in buckets for num in pos] # 1차원화
+        position -= 1
+    return arr
+
+
+num_of_numbers = int(input())
+elements = list(input().split())
+
+max_position = len(max(elements, key=len)) - 1
+
+elements = radix_sort(elements, max_position)
+print(' '.join(map(str, elements)))
