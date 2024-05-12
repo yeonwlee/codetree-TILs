459c3f49cd31c3559d49f9709543515b12ab2fc5
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-12 18:13:58 +0900

    Title: 아름다운 수열 2, Time: 182ms, Memory: 26MB, Status: Passed - CodeTree

--- 240512/아름다운 수열 2/beautiful-sequence-2.py
+++ 240512/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,11 @@
 len_numbers1, len_numbers2 = map(int, input().split())
 
 numbers1 = list(map(int,input().split()))
-numbers2 = list(map(int,input().split()))
+numbers2 = sorted(list(map(int,input().split())))
 answer_count = 0
 
 for start_index in range(len(numbers1) - len(numbers2) + 1):
-    arr_for_checking_pretty_num = numbers2[:]
-    for cur_range_index in range(start_index, start_index + len(numbers2)):
-        if numbers1[cur_range_index] in arr_for_checking_pretty_num:
-            arr_for_checking_pretty_num.remove(numbers1[cur_range_index])
-    if not arr_for_checking_pretty_num:
+    if sorted(numbers1[start_index:start_index+len(numbers2)]) == numbers2:
         answer_count += 1
 
 print(answer_count)
