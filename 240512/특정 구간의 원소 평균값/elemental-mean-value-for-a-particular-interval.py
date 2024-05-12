fcd36d155513c1568ded6aa45b777f24027b4a4f
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-12 17:34:29 +0900

    Title: 특정 구간의 원소 평균값, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240512/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240512/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -1,12 +1,12 @@
 num_of_numbers = int(input())
 numbers = list(map(int, input().split()))
-answer_count = 0 # 각 원소별 하나의 구간으로 두는 경우
+answer_count = 0
 
 for start_index in range(len(numbers)):
-    for end_index in range(start_index + 1, len(numbers)):
-        cur_range_numbers = numbers[start_index:end_index]
+    for end_index in range(start_index, len(numbers)):
+        cur_range_numbers = numbers[start_index:end_index + 1]
         set_cur_range_numbers = set(cur_range_numbers)
-        if sum(cur_range_numbers) // len(cur_range_numbers) in set_cur_range_numbers:
+        if sum(cur_range_numbers) / len(cur_range_numbers) in set_cur_range_numbers:
             answer_count += 1
 
 print(answer_count)
