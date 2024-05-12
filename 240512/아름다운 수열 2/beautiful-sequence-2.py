bdea9ce82d51e13689af18116267554fec945c52
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-12 18:11:31 +0900

    Title: 아름다운 수열 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240512/아름다운 수열 2/beautiful-sequence-2.py
+++ 240512/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,26 +1,15 @@
-from itertools import permutations
-
-
-def is_pretty_number(cur_pretty_index:int, num_for_checking:int) -> bool:
-    for pretty_nums in pretties:
-        if pretty_nums[cur_pretty_index] == num_for_checking:
-            return True
-    return False
-
-
 len_numbers1, len_numbers2 = map(int, input().split())
 
 numbers1 = list(map(int,input().split()))
 numbers2 = list(map(int,input().split()))
-pretties = list(permutations(numbers2, len(numbers2)))
 answer_count = 0
 
 for start_index in range(len(numbers1) - len(numbers2) + 1):
-    pretty_index = 0
+    arr_for_checking_pretty_num = numbers2[:]
     for cur_range_index in range(start_index, start_index + len(numbers2)):
-        if is_pretty_number(pretty_index, numbers1[cur_range_index]):
-            pretty_index += 1
-    if pretty_index == len(numbers2):
+        if numbers1[cur_range_index] in arr_for_checking_pretty_num:
+            arr_for_checking_pretty_num.remove(numbers1[cur_range_index])
+    if not arr_for_checking_pretty_num:
         answer_count += 1
 
 print(answer_count)
