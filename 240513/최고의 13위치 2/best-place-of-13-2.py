4603385d49340e29b87b2cacd5e234bb9afb8f13
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-13 16:49:42 +0900

    Title: 최고의 13위치 2, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240513/최고의 13위치 2/best-place-of-13-2.py
+++ 240513/최고의 13위치 2/best-place-of-13-2.py
@@ -25,10 +25,10 @@ max_num_of_coins = window1 + window2
 window1_row = 0
 window1_col = 1
 while window1_row < height_of_grid:
-    for col_1 in range(window1_col, width_of_grid - 3):
+    for col_1 in range(window1_col, width_of_grid - 2):
         window1 = window1 - grid[window1_row][col_1 - 1] + grid[window1_row][col_1]
         while window2_row < height_of_grid:
-            for col_2 in range(window2_col, width_of_grid - 3):
+            for col_2 in range(window2_col, width_of_grid - 2):
                 window2 = window2 - grid[window2_row][col_2 - 1] + grid[window2_row][col_2]
                 max_num_of_coins = max(max_num_of_coins, window1 + window2)
             window2_row += 1
