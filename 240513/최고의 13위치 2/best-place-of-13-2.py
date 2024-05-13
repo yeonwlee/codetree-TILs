8600d3dd2109f15332f9df627613145f8d2d06e6
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-13 17:07:57 +0900

    Title: 최고의 13위치 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240513/최고의 13위치 2/best-place-of-13-2.py
+++ 240513/최고의 13위치 2/best-place-of-13-2.py
@@ -9,29 +9,24 @@ grid = [
     for _ in range(width_of_grid)
 ]
 
-window1 = sum(grid[0][:3])
-can_be_same_line = are_they_can_be_with_same_line(0, 3)
-if can_be_same_line:
-    window2 = sum(grid[0][3:6])
-    window2_row = 0
-    window2_col = 4
-else:
-    window2 = sum(grid[1][:3])
-    window2_row = 1
-    window2_col = 1
-
-max_num_of_coins = window1 + window2
+max_num_of_coins = 0
 
 window1_row = 0
-window1_col = 1
 while window1_row < height_of_grid:
     window1 = sum(grid[window1_row][:3])
     window1_col = 1
+    if are_they_can_be_with_same_line(0, 3):
+        window2_row = window1_row
+        window2_col = 3
+    else:
+        window2_row = window1_row + 1
+        window2_col = 0
+    if window2_row < height_of_grid:
+        window2 = sum(grid[window2_row][window2_col:window2_col+3])
+        window2_col += 1
+    max_num_of_coins = max(max_num_of_coins, window1 + window2)
     for col_1 in range(window1_col, width_of_grid - 2):
         window1 = window1 - grid[window1_row][col_1 - 1] + grid[window1_row][col_1]
-        if window2_row < height_of_grid:
-            window2 = sum(grid[window2_row][window2_col:window2_col+3])
-            window2_col += 1
         while window2_row < height_of_grid:
             for col_2 in range(window2_col, width_of_grid - 2):
                 window2 = window2 - grid[window2_row][col_2 - 1] + grid[window2_row][col_2]
@@ -39,11 +34,6 @@ while window1_row < height_of_grid:
             window2_row += 1
             window2_col = 0
     window1_row += 1
-    if can_be_same_line:
-        window2_row = window1_row
-        window2_col = 3
-    else:
-        window2_row = window1_row + 1
-        window2_col = 0
+   
 
 print(max_num_of_coins)
