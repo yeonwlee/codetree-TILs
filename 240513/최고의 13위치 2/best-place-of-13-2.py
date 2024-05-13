a2e62698ab4df2866803656c19f96fd34104f508
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-13 17:42:15 +0900

    Title: 최고의 13위치 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240513/최고의 13위치 2/best-place-of-13-2.py
+++ 240513/최고의 13위치 2/best-place-of-13-2.py
@@ -5,17 +5,16 @@ grid = [
     for _ in range(width_of_grid)
 ]
 
+sum_grid = [[sum(row[i:i+3]) for i in range(width_of_grid - 2)] for row in grid]
 max_num_of_coins = 0
 
 for row_1 in range(height_of_grid):
     for col_1 in range(width_of_grid - 2):
-        target1= sum(grid[row_1][col_1:col_1+3])
         for row_2 in range(row_1, height_of_grid):
             start_col = col_1 + 3 if row_1 == row_2 else 0
             if start_col + 3 <= width_of_grid:   
-                for col_2 in range(start_col, width_of_grid - 2):
-                    target2 = sum(grid[row_2][col_2:col_2+3])
-                    max_num_of_coins = max(max_num_of_coins, target1 + target2)
+                for col_2 in range(start_col, width_of_grid - 2): 
+                    max_num_of_coins = max(max_num_of_coins, sum_grid[row_1][col_1]+sum_grid[row_2][col_2])
             else:
                 continue
    
