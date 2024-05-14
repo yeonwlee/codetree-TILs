3effb2281279354c95233280b5bbdbc0f380002d
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 16:13:25 +0900

    Title: 밭의 높이를 고르게하기, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -2,21 +2,11 @@ num_of_field, target_height, min_num_of_connective_field_same_height = map(int, 
 
 fields = list(map(int, input().split()))
 
-# 미리 연속 타겟 묶음으로 높이를 더해둠
-fields_height_sum = [
-    sum(fields[index:index + min_num_of_connective_field_same_height]) 
-    for index in range(num_of_field - min_num_of_connective_field_same_height + 1)
-]
-
 min_cost = float('inf')
-target_sum = target_height * min_num_of_connective_field_same_height
-
-for index, cur_range_sum in enumerate(fields_height_sum):
-    cur_cost = abs(cur_range_sum - target_sum)
-    if cur_cost == 0:
-        cur_cost = sum(abs(target_height - element) 
-            for element in fields[index:index+min_num_of_connective_field_same_height] 
-            if element != target_height)
+for index in range(len(fields) - min_num_of_connective_field_same_height + 1):
+    cur_cost = 0
+    for in_range_index in range(index, index + min_num_of_connective_field_same_height):
+        cur_cost += abs(target_height - fields[in_range_index])
     min_cost = min(min_cost, cur_cost)
 
 print(min_cost)
