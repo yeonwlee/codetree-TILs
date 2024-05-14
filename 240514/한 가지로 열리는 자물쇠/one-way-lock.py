dbb2baf30e6b4155bfd330fb3589d6672bd3e5c4
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 16:36:26 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 403ms, Memory: 27MB, Status: Passed - CodeTree

--- 240514/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240514/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -18,17 +18,9 @@ access_difference = 2 # 한 자리라도 2 이내로 차이가 나야함
 
 answer_count = 0
 for num1 in range(1, num_max_range + 1):
-    if abs(num1 - target_combination[0]) <= 2:
-        answer_count += (num_max_range * num_max_range)
-    else:
-        for num2 in range(1, num_max_range + 1):
-            if abs(num2 - target_combination[1]) <= 2:
-                answer_count += num_max_range
-            else:
-                for num3 in range(1, num_max_range + 1):
-                    if abs(num3 - target_combination[2]) <= 2:
-                        answer_count += 1
-                    else:
-                        break
+    for num2 in range(1, num_max_range + 1):
+        for num3 in range(1, num_max_range + 1):
+            if any(abs(cur_value - condition_value) <= 2 for cur_value, condition_value in zip([num1, num2, num3], target_combination)):
+                answer_count += 1
 
 print(answer_count)
