c8718cf1333b286c0c3f47142628cb3c97d79740
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 16:07:38 +0900

    Title: 밭의 높이를 고르게하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,14 +1,22 @@
-num_of_feild, target_height, minimum_num_of_connective_field_same_height = map(int, input().split())
+num_of_field, target_height, min_num_of_connective_field_same_height = map(int, input().split())
 
 fields = list(map(int, input().split()))
+
 # 미리 연속 타겟 묶음으로 높이를 더해둠
-fields_height_sum = [sum(fields[index:index+minimum_num_of_connective_field_same_height]) for index in range(num_of_feild - minimum_num_of_connective_field_same_height + 1)]
+fields_height_sum = [
+    sum(fields[index:index + min_num_of_connective_field_same_height]) 
+    for index in range(num_of_field - min_num_of_connective_field_same_height + 1)
+]
 
-minimum_cost = float('inf')
+min_cost = float('inf')
+target_sum = target_height * min_num_of_connective_field_same_height
+
 for index, cur_range_sum in enumerate(fields_height_sum):
-    cur_cost = abs(cur_range_sum - (target_height * minimum_num_of_connective_field_same_height))
+    cur_cost = abs(cur_range_sum - target_sum)
     if cur_cost == 0:
-        cur_cost = sum(abs(target_height - element) for element in fields[index:index+minimum_num_of_connective_field_same_height] if element != target_height)
-    minimum_cost = min(minimum_cost, cur_cost)
+        cur_cost = sum(abs(target_height - element) 
+            for element in fields[index:index+min_num_of_connective_field_same_height] 
+            if element != target_height)
+    min_cost = min(min_cost, cur_cost)
 
-print(minimum_cost)
+print(min_cost)
