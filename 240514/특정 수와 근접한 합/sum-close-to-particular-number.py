b93144308e6d57cbed156769084b46918b40d60c
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 13:00:44 +0900

    Title: 특정 수와 근접한 합, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240514/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -13,6 +13,6 @@ difference_value_with_target_num = 10001
 for except_num1_index in range(num_of_numbers):
     sum_except_num1 = cumulative_sum_arr[-1] - numbers[except_num1_index]
     for except_num2_index in range(except_num1_index + 1, num_of_numbers):
-        difference_value_with_target_num = min(difference_value_with_target_num, sum_except_num1 - numbers[except_num2_index])
+        difference_value_with_target_num = min(difference_value_with_target_num, abs(target_num - (sum_except_num1 - numbers[except_num2_index])))
 
-print(difference_value_with_target_num - target_num)
+print(difference_value_with_target_num)
