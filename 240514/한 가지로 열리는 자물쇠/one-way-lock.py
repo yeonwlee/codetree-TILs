63ad6d41b03b76b953968cac4fc1699a05146636
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 16:26:29 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 591ms, Memory: 29MB, Status: Passed - CodeTree

--- 240514/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240514/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,12 +1,26 @@
-from itertools import product
+##### product를 사용한 풀이
+# from itertools import product
+
+# num_max_range = int(input())
+# target_combination = tuple(map(int, input().split()))
+# access_difference = 2 # 한 자리라도 2 이내로 차이가 나야함
+
+# answer_count = 0
+# for cur_combination in product([num for num in range(1, num_max_range + 1)], repeat=3):
+#     if any(1 for cur_value, condition_value in zip(cur_combination, target_combination) if abs(condition_value - cur_value) <= 2):
+#         answer_count += 1
+
+# print(answer_count)
 
 num_max_range = int(input())
 target_combination = tuple(map(int, input().split()))
 access_difference = 2 # 한 자리라도 2 이내로 차이가 나야함
 
 answer_count = 0
-for cur_combination in product([num for num in range(1, num_max_range + 1)], repeat=3):
-    if any(1 for cur_value, condition_value in zip(cur_combination, target_combination) if abs(condition_value - cur_value) <= 2):
-        answer_count += 1
+for num1 in range(1, num_max_range + 1):
+    for num2 in range(1, num_max_range + 1):
+        for num3 in range(1, num_max_range + 1):
+            if any(1 for cur_value, condition_value in zip([num1, num2, num3], target_combination) if abs(cur_value - condition_value) <= 2):
+                answer_count += 1
 
 print(answer_count)
