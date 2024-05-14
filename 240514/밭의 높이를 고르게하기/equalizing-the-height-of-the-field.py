d3e472f7692661aeb60cf69be1f7feb680f7b7d3
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 16:03:22 +0900

    Title: 밭의 높이를 고르게하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240514/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -8,7 +8,7 @@ minimum_cost = float('inf')
 for index, cur_range_sum in enumerate(fields_height_sum):
     cur_cost = abs(cur_range_sum - (target_height * minimum_num_of_connective_field_same_height))
     if cur_cost == 0:
-        cur_cost = sum(abs(target_height - element) for element in fields[index:index+3] if element != target_height)
+        cur_cost = sum(abs(target_height - element) for element in fields[index:index+minimum_num_of_connective_field_same_height] if element != target_height)
     minimum_cost = min(minimum_cost, cur_cost)
 
 print(minimum_cost)
