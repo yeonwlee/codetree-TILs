32c556a47f9389e5a9fb62a048eac818c2665fab
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-14 12:59:21 +0900

    Title: 특정 수와 근접한 합, Time: 118ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240514/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,18 @@
+num_of_numbers, target_num = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+# 누적합 만들기
+# cumulative_sum = list(itertools.accumulate(numbers))
+cumulative_sum_arr = []
+current_sum = 0
+for num in numbers:
+    current_sum += num
+    cumulative_sum_arr.append(current_sum)
+
+difference_value_with_target_num = 10001
+for except_num1_index in range(num_of_numbers):
+    sum_except_num1 = cumulative_sum_arr[-1] - numbers[except_num1_index]
+    for except_num2_index in range(except_num1_index + 1, num_of_numbers):
+        difference_value_with_target_num = min(difference_value_with_target_num, sum_except_num1 - numbers[except_num2_index])
+
+print(difference_value_with_target_num - target_num)
