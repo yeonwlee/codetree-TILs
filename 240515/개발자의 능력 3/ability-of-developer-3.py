94dd96ff189eab75388c75b3cb1be3e6842696de
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-15 16:06:49 +0900

    Title: 개발자의 능력 3, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/개발자의 능력 3/ability-of-developer-3.py
+++ 240515/개발자의 능력 3/ability-of-developer-3.py
@@ -9,8 +9,8 @@ sum_whole_capability = sum(devs)
 min_difference = float('inf')
 
 for first_dev_index in range(len(devs) - 2):
-    for second_dev_index in range(first_dev_index, len(devs) - 1):
-        for third_dev_index in range(second_dev_index, len(devs)):
+    for second_dev_index in range(first_dev_index + 1, len(devs) - 1):
+        for third_dev_index in range(second_dev_index + 1, len(devs)):
             min_difference = min(min_difference, 
                                 get_difference(sum([devs[first_dev_index], devs[second_dev_index], devs[third_dev_index]]))
                             )
