a9c9f73711cd78679311e32d537e90690670dd4f
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-15 17:51:18 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 520ms, Memory: 29MB, Status: Passed - CodeTree

--- 240515/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240515/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,19 +1,15 @@
-def find_valid_value(cur_value:int) -> set[int]:
-    valid_set = set([cur_value])
-    for value in range(-2, 3):
-        valid_set.add((cur_value + value - 1) % max_number_of_range + 1)
-    return valid_set
+def is_within_range(value, target):
+    return abs(value - target) <= 2 or abs(value - target) >= max_number_of_range - 2
 
-def check_valid_combi(valid_nums:list[set[int]], cur_combi:tuple[int]) -> bool:
-    return all(cur_value in valid_num_set for valid_num_set, cur_value in zip(valid_nums, cur_combi))
+
+def is_valid_combi(combi, first_combi, second_combi):
+    return (all(is_within_range(combi[i], first_combi[i]) for i in range(3)) 
+            or all(is_within_range(combi[i], second_combi[i]) for i in range(3)))
 
 
 max_number_of_range = int(input())
 first_combi = tuple(map(int, input().split()))
 second_combi = tuple(map(int, input().split()))
-
-valid_num_at_first_combi = [find_valid_value(num) for num in first_combi]
-valid_num_at_second_combi = [find_valid_value(num) for num in second_combi]
 
 num_of_valid_combi = 0
 
@@ -21,7 +17,7 @@ for first in range(1, max_number_of_range + 1):
     for second in range(1, max_number_of_range + 1):
         for third in range(1, max_number_of_range + 1):
             cur_combi = (first, second, third)
-            if check_valid_combi(valid_num_at_first_combi, cur_combi) or check_valid_combi(valid_num_at_second_combi, cur_combi):
+            if is_valid_combi(cur_combi, first_combi, second_combi):
                 num_of_valid_combi += 1
 
 
