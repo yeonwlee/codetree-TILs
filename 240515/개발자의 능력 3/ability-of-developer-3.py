5fabc9836a4376b5280778736798a253457e555f
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-15 16:04:58 +0900

    Title: 개발자의 능력 3, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240515/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,18 @@
+def get_difference(team1:int) -> int:
+    team2 = sum_whole_capability - team1 
+    return abs(team2 - team1)
+
+
+# 6명을 3명씩 2팀으로 배정. 능력 총합 차이 최소화
+devs = list(map(int, input().split()))
+sum_whole_capability = sum(devs)
+min_difference = float('inf')
+
+for first_dev_index in range(len(devs) - 2):
+    for second_dev_index in range(first_dev_index, len(devs) - 1):
+        for third_dev_index in range(second_dev_index, len(devs)):
+            min_difference = min(min_difference, 
+                                get_difference(sum([devs[first_dev_index], devs[second_dev_index], devs[third_dev_index]]))
+                            )
+
+print(min_difference)
