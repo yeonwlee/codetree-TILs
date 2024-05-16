303f05daf4b0139fbe5403e165717f3718e44639
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-16 17:33:44 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240516/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240516/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -5,17 +5,14 @@ points = [
     for _ in range(num_of_points)
 ]
 
-min_distance = float('inf')
-min_distance_pow = 0
+min_distance_pow = float('inf')
 
 for first_index in range(num_of_points - 1):
     for second_index in range(first_index + 1, num_of_points):
         x_distance = abs(points[first_index][0] - points[second_index][0])
         y_distance = abs(points[first_index][1] - points[second_index][1])
-        cur_distance = x_distance + y_distance
         cur_distance_pow = x_distance ** 2 + y_distance ** 2
-        if cur_distance < min_distance or (cur_distance == min_distance and cur_distance_pow < min_distance_pow):
-            min_distance = cur_distance
+        if cur_distance_pow < min_distance_pow:
             min_distance_pow = cur_distance_pow
 
 print(min_distance_pow)
