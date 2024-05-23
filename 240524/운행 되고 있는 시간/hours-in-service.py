1104ce08c63ed764c4d3382d1961735144ae847e
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-24 00:57:58 +0900

    Title: 운행 되고 있는 시간, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240524/운행 되고 있는 시간/hours-in-service.py
+++ 240524/운행 되고 있는 시간/hours-in-service.py
@@ -12,8 +12,8 @@ for index in range(len(dev_working_time)):
     remain_devs = dev_working_time[:index] + dev_working_time[index+1:]
     for remain in remain_devs:
         start, end = remain
-        for cur_working_time in range(start, end + 1):
+        for cur_working_time in range(start, end): # 마지막 시간이 포함되지 않게
             check_working_time[cur_working_time] += 1
     max_sum_working_time = max(max_sum_working_time, sum(1 if working_time >= 1 else 0 for working_time in check_working_time))
 
-print(max_sum_working_time - 2) # 마지막 시간이 포함되지 않게
+print(max_sum_working_time)
