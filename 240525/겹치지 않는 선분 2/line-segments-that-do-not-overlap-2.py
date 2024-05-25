92238d2dc23dcc2d94724c8f4f86b7b39253b5c1
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-25 21:34:23 +0900

    Title: 겹치지 않는 선분 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240525/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -2,12 +2,6 @@
 # (선분2의 시작점 < 선분1의 시작점 and 선분2의 끝점 < 선분1의 끝점) or
 # (선분1의 시작점 < 선분2의 시작점 and 선분1의 끝점 < 선분2의 끝점)
 
-def is_cross_eachother(line1:tuple[int], line2:tuple[int]) -> bool:
-    start1, end1 = line1
-    start2, end2 = line2
-    return not ((start2 < start1 and end2 < end1) or (start1 < start2 and end1 < end2))
-
-
 num_of_lines = int(input())
 
 lines = [
@@ -19,11 +13,13 @@ checked_lines = [0] * num_of_lines
 num_of_lines_not_crossing = 0
 
 for line_for_checking_index in range(num_of_lines):
-    if not checked_lines[line_for_checking_index]:
+    if not checked_lines[line_for_checking_index]: # 이미 다른 선분과 겹쳤던 경우 생략
         for other_line_index in range(num_of_lines):
-            if line_for_checking_index == other_line_index:
+            if line_for_checking_index == other_line_index: # 자기 자신은 생략
                 continue
-            if is_cross_eachother(lines[line_for_checking_index], lines[other_line_index]):
+            start1, end1 = lines[line_for_checking_index]
+            start2, end2 = lines[other_line_index]
+            if not ((start2 < start1 and end2 < end1) or (start1 < start2 and end1 < end2)): # 평행하지 않으면(=겹치면)
                 checked_lines[line_for_checking_index], checked_lines[other_line_index] = 1, 1
                 break
         else:
