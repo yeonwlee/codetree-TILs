8840daf79902e268a78acc1872056fb0b2293027
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-25 23:32:46 +0900

    Title: 스승의 은혜 2, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240525/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,20 @@
+num_of_students, budget = map(int, input().split())
+
+student_present_info = [
+    int(input())
+    for _ in range(num_of_students)
+]
+
+num_of_whole_students = len(student_present_info)
+student_present_info.sort(reverse=True)
+
+while budget > 0:
+    if budget - student_present_info[-1] < 0 and budget - student_present_info[-1] / 2 >= 0:
+        student_present_info.pop()
+        break
+    elif budget - student_present_info[-1] >= 0:
+        budget -= student_present_info.pop()
+    else:
+        break
+
+print(num_of_whole_students - len(student_present_info))
