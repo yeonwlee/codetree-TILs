44e7352767e2ea432a1a8c2c6339b8b2cdb8f583
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-25 23:10:23 +0900

    Title: 숫자 카운트, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240525/숫자 카운트/numeric-count.py
+++ 240525/숫자 카운트/numeric-count.py
@@ -7,19 +7,18 @@ try_info = [
     for _ in range(try_num)
 ]
 
-all_combination = set()
+possible_combinations = set()
 for first in range(1, 10):
     for second in range(1, 10):
         for third in range(1, 10):
-            all_combination.add((first, second, third))
+            possible_combinations.add((first, second, third))
 
-possible_combinations = all_combination.copy()
 
 for trying in try_info:
     suggest_answer, count_num_at_the_position, count_num_in_set = trying 
     suggest_answer = list(map(int, str(suggest_answer)))
     possible_combinations = {combi for combi in possible_combinations
-                            if count_num_at_the_position == sum(1 for index in range(3) if combi[index] == suggest_answer[index])
-                            if count_num_in_set == sum(1 for index in range(3) if combi[index] != suggest_answer[index] and suggest_answer[index] in combi)}
+                            if count_num_at_the_position == sum(1 for index in range(3) if combi[index] == suggest_answer[index]) 
+                            and count_num_in_set == sum(1 for index in range(3) if combi[index] != suggest_answer[index] and suggest_answer[index] in combi)}
 
 print(len(possible_combinations))
