940a4bbe961f3cf4b23eeb10cef5951d20c7f08b
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-26 17:29:19 +0900

    Title: 스승의 은혜 3, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240526/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240526/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,18 +7,19 @@ student_present_info = [
 student_present_info.sort(key=lambda x: [x[0] + x[1], x[1]])
 max_student_num = 0
 
-for student_index in range(len(student_present_info) - 1, -1, -1):
+# 모든 학생의 선물 값에 쿠폰을 적용해보기
+for student_index in range(len(student_present_info)):
     price, send_fee = student_present_info[student_index]
-    apply_coupon_price = price // 2 + send_fee
+    apply_coupon_price = price // 2 + send_fee 
     sum_price = apply_coupon_price
     students = 1
-    for student_index2 in range(len(student_present_info)):
-        if student_index == student_index2:
+    for student_index2 in range(len(student_present_info)): 
+        if student_index == student_index2: # 그 외 학생들
             continue
-        sum_price += sum(student_present_info[student_index2])
+        sum_price += sum(student_present_info[student_index2]) # 쿠폰 적용 안 한 값을 적용
         if sum_price >= budget:
             max_student_num = max(max_student_num, students)
             break
         students += 1
-        
+
 print(max_student_num)
