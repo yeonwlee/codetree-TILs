afad3abe781f3f7ec71162b76bd27412fae49178
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-26 18:44:11 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240526/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,15 @@
+def set_numbers(cur_answer:list[int]):
+    if len(cur_answer) == num_of_choice:
+        print(' '.join(map(str, cur_answer)))
+        return
+    for num in range(1, maximum_value + 1):
+        cur_answer.append(num)
+        set_numbers(cur_answer)
+        cur_answer.pop()
+    
+
+
+num_of_choice, maximum_value = map(int, input().split())
+
+for num in range(1, maximum_value + 1):
+    set_numbers([num])
