ac5d6a51258609e61f7cd6238c8dad1fbaeeeeb1
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-26 14:46:07 +0900

    Title: 개발자의 순위, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240526/개발자의 순위/developer's-rank.py
+++ 240526/개발자의 순위/developer's-rank.py
@@ -6,14 +6,14 @@ result_of_compitition = [
 ]
 
 # 자기 외 다른 개발자들을 모두 하위 순위 개발자로 초기화
-result = {dev:{dev_num for dev_num in range(1, num_of_dev + 1) if dev != dev_num} for dev in range(1, num_of_dev + 1)}
+result = {dev:set(range(1, num_of_dev + 1)) - {dev} for dev in range(1, num_of_dev + 1)}
 
 for compitition in result_of_compitition:
-    for target_index in range(len(compitition)):
+    for target_index, dev in enumerate(compitition):
         # 하위 순위의 개발자들
-        lower_devs = {compitition[compare_index] for compare_index in range(target_index + 1, len(compitition))}
+        lower_devs = set(compitition[target_index + 1:])
         # 교집합
-        result[compitition[target_index]] = result[compitition[target_index]] & lower_devs
+        result[dev] &= lower_devs
 
 case_count = sum(len(lowers) for lowers in result.values())
 print(case_count)
