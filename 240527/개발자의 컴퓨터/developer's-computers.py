d59ca0b97a9b1b38bda8cebf4bcaad66f2efa4b1
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-27 15:05:50 +0900

    Title: 개발자의 컴퓨터, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240527/개발자의 컴퓨터/developer's-computers.py
+++ 240527/개발자의 컴퓨터/developer's-computers.py
@@ -1,14 +1,18 @@
-num_of_dev = int(input())
+from collections import Counter
+
+num_of_dev = int(input()) # 개발자의 수
 
 dev_working = [
     tuple(map(int, input().split()))
     for _ in range(num_of_dev)
-]
+] # 시작 시간, 끝시간, 필요한 컴퓨터의 수
 
-time = [0] * 1001
+# Counter 객체로 필요한 만큼만 메모리 사용
+time = Counter()
 
-for start, end, require_com in dev_working:
+for start, end, required_computer in dev_working:
     for working_time in range(start, end + 1):
-        time[working_time] += require_com
+        time[working_time] += required_computer
 
-print(max(time))
+# 최대 필요한 컴퓨터의 수
+print(max(time.values()))
