39aeee0079b8d6e1c5cb1d3ac927b910aafe6f67
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-27 14:01:08 +0900

    Title: 개발자의 능력 2, Time: 118ms, Memory: 28MB, Status: Passed - CodeTree

--- 240527/개발자의 능력 2/ability-of-developer-2.py
+++ 240527/개발자의 능력 2/ability-of-developer-2.py
@@ -1,11 +1,36 @@
 # 팀원들의 능력 총합이 가장 큰 팀과 가장 작은 팀의 차이 최소화 -> 되도록 평균치에 가깝게 
 # 정렬해서 양 끝에서부터 주면 안 되나? -- 예외적인 경우가 있을까?
+# 풀이1
+# devs = list(map(int, input().split()))
+
+# devs.sort()
+# teams = []
+
+# for index in range(len(devs) // 2):
+#     teams.append(devs[index] + devs[len(devs) - 1 - index])
+
+# print(max(teams) - min(teams))
+
+
+## 풀이2: 조합 활용
+from itertools import combinations
+
 devs = list(map(int, input().split()))
 
-devs.sort()
-teams = []
+dev_combi = list(map(set, combinations(range(6), 2)))
+difference = float('inf')
 
-for index in range(len(devs) // 2):
-    teams.append(devs[index] + devs[len(devs) - 1 - index])
+for combi in dev_combi:
+    for combi2 in dev_combi:
+        if combi & combi2:
+            continue
+        for combi3 in dev_combi:
+            if combi & combi3 or combi2 & combi3:
+                continue
+            sums = [sum(devs[x] for x in combi), sum(devs[x] for x in combi2), sum(devs[x] for x in combi3)]  
+            best = max(sums)
+            worst = min(sums)
+            difference = min(difference, best - worst)
 
-print(max(teams) - min(teams))
+
+print(difference)
