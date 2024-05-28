7129e1a491ac4ddd34378651b1b057ceb156bd83
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-28 12:15:54 +0900

    Title: 이상한 폭탄 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240528/이상한 폭탄 2/strange-bomb-2.py
+++ 240528/이상한 폭탄 2/strange-bomb-2.py
@@ -36,7 +36,7 @@ bombs = [
 # 폭탄의 번호는 0부터 시작. 폭발할 폭탄 중 번호가 가장 큰 번호를 출력. 없으면 -1
 max_bomb_num = -1
     
-for start_bomb_index in range(num_of_bombs - explosion_distance + 1):
+for start_bomb_index in range(num_of_bombs):
     for compare_bomb_index in range(start_bomb_index + 1, min(start_bomb_index + 1 + explosion_distance, num_of_bombs)):
         if bombs[start_bomb_index] == bombs[compare_bomb_index]:
             max_bomb_num = max(max_bomb_num, bombs[start_bomb_index]) # 폭발할 폭탄 중 번호가 가장 큰 번호 갱신
