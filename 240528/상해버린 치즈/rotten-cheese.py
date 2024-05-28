a43d09bd168df40cc9912c3719bc1553b8c11fe2
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-28 13:12:02 +0900

    Title: 상해버린 치즈, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240528/상해버린 치즈/rotten-cheese.py
+++ 240528/상해버린 치즈/rotten-cheese.py
@@ -23,7 +23,8 @@ for sick_info in sick_record:
     person, sick_time = sick_info # index 아님
     suspect_cheese[person].update(cheese for eater, cheese, eating_time in eating_cheese_record if eater == person and eating_time < sick_time)
 
+# 아, 한 사람이 여러 번 먹을 수 있음.
 for maybe_rot_cheese in reduce(lambda x, y: x & y, suspect_cheese.values()):
-    max_num_of_sick_person = max(max_num_of_sick_person, sum(1 for _, cheese, _ in eating_cheese_record if cheese == maybe_rot_cheese))
+    max_num_of_sick_person = max(max_num_of_sick_person, len({eater for eater, cheese, _ in eating_cheese_record if cheese == maybe_rot_cheese}))
 
 print(max_num_of_sick_person)
