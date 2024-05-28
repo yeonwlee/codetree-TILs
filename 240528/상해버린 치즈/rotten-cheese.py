9322566654f4d552995fbe8f4ab5f1683baa2517
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-28 13:06:19 +0900

    Title: 상해버린 치즈, Time: 154ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/상해버린 치즈/rotten-cheese.py
+++ 240528/상해버린 치즈/rotten-cheese.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from functools import reduce 
+
 num_of_person, num_of_cheese, num_of_eating_cheese_record, num_of_sick_record = map(int, input().split())
 
 eating_cheese_record = [
@@ -10,17 +13,17 @@ sick_record = [
     for _ in range(num_of_sick_record)
 ]
 
-suspect_cheese = set()
+suspect_cheese = defaultdict(set)
 max_num_of_sick_person = 0
+
 # 치즈는 '하나만' 상했음
 # 아픈 사람들이 아프기 시작한 시간 전(최소 -1)에 먹은 치즈가 상했을 수 있음
 # 의심되는 치즈를 먹은 사람들의 최대 수 
-
 for sick_info in sick_record:
     person, sick_time = sick_info # index 아님
-    suspect_cheese.update([cheese for eater, cheese, eating_time in eating_cheese_record if eater == person and eating_time < sick_time])
+    suspect_cheese[person].update(cheese for eater, cheese, eating_time in eating_cheese_record if eater == person and eating_time < sick_time)
 
-for maybe_rot_cheese in suspect_cheese:
+for maybe_rot_cheese in reduce(lambda x, y: x & y, suspect_cheese.values()):
     max_num_of_sick_person = max(max_num_of_sick_person, sum(1 for _, cheese, _ in eating_cheese_record if cheese == maybe_rot_cheese))
 
 print(max_num_of_sick_person)
