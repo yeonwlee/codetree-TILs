14dd67fa7069c72a5dd840d7c69302d36d0a7e37
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-28 13:47:08 +0900

    Title: 숫자들의 합 중 최대, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240528/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 240528/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -1,4 +1,5 @@
 # start_num 이상 end_num 이하
 start_num, end_num = map(int, input().split())
 
+# 범위내의 숫자의 각 자리수를 합한 것 중 가장 큰 값 출력
 print(max([sum(map(int, str(number))) for number in range(start_num, end_num + 1)]))
