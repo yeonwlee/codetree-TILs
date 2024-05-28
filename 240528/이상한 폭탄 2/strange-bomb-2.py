94c1343174028166ae990db40c3a39f2e86e541a
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-28 11:56:22 +0900

    Title: 이상한 폭탄 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/이상한 폭탄 2/strange-bomb-2.py
+++ 240528/이상한 폭탄 2/strange-bomb-2.py
@@ -1,6 +1,31 @@
-from collections import defaultdict
-from itertools import combinations
+## 폭탄 번호 별 거리 저장 후 풀이
+# from collections import defaultdict
+# from itertools import combinations
 
+# num_of_bombs, explosion_distance = map(int, input().split())
+
+# bombs = [
+#     int(input())
+#     for _ in range(num_of_bombs)
+# ]
+
+# bomb_position = defaultdict(set)
+# # {폭탄번호: {인덱스},..}
+# for index, bomb in enumerate(bombs):
+#     bomb_position[bomb].add(index)
+
+# # 폭탄의 번호는 0부터 시작. 폭발할 폭탄 중 번호가 가장 큰 번호를 출력. 없으면 -1
+# max_bomb_num = -1
+# for bomb_num, bomb_pos in bomb_position.items():
+#     if len(bomb_pos) >= 2: # 같은 폭탄의 번호가 2개 이상이면
+#         for bomb1, bomb2 in combinations(bomb_pos, 2): # 각 거리(인덱스 별) 거리 조합
+#             if abs(bomb1 - bomb2) <= explosion_distance:
+#                 max_bomb_num = max(max_bomb_num, bomb_num) # 폭발할 폭탄 중 번호가 가장 큰 번호 갱신
+
+# print(max_bomb_num)
+
+
+##### 풀이2
 num_of_bombs, explosion_distance = map(int, input().split())
 
 bombs = [
@@ -8,17 +33,12 @@ bombs = [
     for _ in range(num_of_bombs)
 ]
 
-bomb_position = defaultdict(set)
-# {폭탄번호: {인덱스},..}
-for index, bomb in enumerate(bombs):
-    bomb_position[bomb].add(index)
-
 # 폭탄의 번호는 0부터 시작. 폭발할 폭탄 중 번호가 가장 큰 번호를 출력. 없으면 -1
 max_bomb_num = -1
-for bomb_num, bomb_pos in bomb_position.items():
-    if len(bomb_pos) >= 2: # 같은 폭탄의 번호가 2개 이상이면
-        for bomb1, bomb2 in combinations(bomb_pos, 2): # 각 거리(인덱스 별) 거리 조합
-            if abs(bomb1 - bomb2) <= explosion_distance:
-                max_bomb_num = max(max_bomb_num, bomb_num) # 폭발할 폭탄 중 번호가 가장 큰 번호 갱신
+
+for start_bomb_index in range(num_of_bombs - explosion_distance):
+    for compare_bomb_index in range(start_bomb_index + 1, start_bomb_index + explosion_distance + 1):
+        if bombs[start_bomb_index] == bombs[compare_bomb_index]:
+            max_bomb_num = max(max_bomb_num, bombs[start_bomb_index]) # 폭발할 폭탄 중 번호가 가장 큰 번호 갱신
 
 print(max_bomb_num)
