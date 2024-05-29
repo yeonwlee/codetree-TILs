ef05e63b23459c1e0011879eabd5bf811c738baf
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-29 18:48:00 +0900

    Title: 정보에 따른 숫자 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240529/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240529/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -21,11 +21,12 @@ for _ in range(num_of_alphabets):
     alphabet, position = input().split()
     alphabet_pos_info[alphabet].append(int(position))
 
-# 알파벳 위치 정보 정렬 -- 이진 탐색 활용
+# 알파벳 위치 정보 정렬 -- 이진 탐색 활용 위함
 for alphabet, positions in alphabet_pos_info.items():
     alphabet_pos_info[alphabet] = sorted(positions)
 
-# 주어지 범위 내에서 가장 가까운 S의 위치가 가장 가까운 N의 위치보다 더 가까울 경우 찾기
+# 주어진 범위 내에서 
+# 가장 가까운 S의 위치가 가장 가까운 N의 위치보다 더 가까울 경우 찾기
 num_of_special_position = 0
 for cur_checking_pos in range(x1, x2 + 1):
     nearest_s_distance = find_nearest_alphabet_distance(cur_checking_pos, 'S')
