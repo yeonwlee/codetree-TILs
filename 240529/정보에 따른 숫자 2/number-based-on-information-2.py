e6144b005119389f134d568652f3b6da0dbbaf27
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-29 18:41:03 +0900

    Title: 정보에 따른 숫자 2, Time: 166ms, Memory: 24MB, Status: Passed - CodeTree

--- 240529/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240529/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,5 +1,16 @@
-def find_nearest_alphabet_position(cur_pos:int, target:str) -> int:
-    return min(abs(cur_pos - alphabet_pos) for alphabet_pos in alphabet_pos_info[target])
+from bisect import bisect_left # 이진 탐색
+
+
+def find_nearest_alphabet_distance(cur_pos:int, target:str) -> int:
+    alphabet_positions = alphabet_pos_info[target]
+    compare_pos = bisect_left(alphabet_positions, cur_pos)
+
+    if compare_pos == 0:
+        return abs(cur_pos - alphabet_positions[0])
+    elif compare_pos == len(alphabet_positions):
+        return abs(cur_pos - alphabet_positions[-1])
+    else:
+        return min(abs(cur_pos - alphabet_positions[compare_pos - 1]), abs(cur_pos - alphabet_positions[compare_pos]))
 
 
 num_of_alphabets, x1, x2 = map(int, input().split())
@@ -15,7 +26,7 @@ for alphabet, positions in alphabet_pos_info.items():
 
 num_of_special_position = 0
 for cur_checking_pos in range(x1, x2 + 1):
-    if find_nearest_alphabet_position(cur_checking_pos, 'S') <= find_nearest_alphabet_position(cur_checking_pos, 'N'):
+    if find_nearest_alphabet_distance(cur_checking_pos, 'S') <= find_nearest_alphabet_distance(cur_checking_pos, 'N'):
         num_of_special_position += 1
 
 print(num_of_special_position)
