6cffb2b168b09c560c581d13386cf0c97acada64
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-29 18:26:32 +0900

    Title: 정보에 따른 숫자 2, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240529/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240529/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -13,11 +13,8 @@ for _ in range(num_of_alphabets):
 for alphabet, positions in alphabet_pos_info.items():
     alphabet_pos_info[alphabet] = sorted(positions)
 
-# S가 있는 위치 중 가장 작은 값과 큰 값이 범위의 시작과 끝.
-start, end = alphabet_pos_info['S'][0], alphabet_pos_info['S'][-1]
-
 num_of_special_position = 0
-for cur_checking_pos in range(start, end + 1):
+for cur_checking_pos in range(x1, x2 + 1):
     if find_nearest_alphabet_position(cur_checking_pos, 'S') <= find_nearest_alphabet_position(cur_checking_pos, 'N'):
         num_of_special_position += 1
 
