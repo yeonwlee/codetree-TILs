2447e975d4fcc8011a9e0f93f37240f4e626e9fc
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-29 16:50:41 +0900

    Title: 흥미로운 숫자 2, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240529/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240529/흥미로운 숫자 2/interesting-numbers-2.py
@@ -6,18 +6,18 @@ low, high = map(int, input().split())
 
 low_num_of_pos = len(list(map(int, str(low))))
 high_num_of_pos = len(list(map(int, str(high))))
-suspect_numbers = []
+suspect_numbers = set()
 
 for digit in range(low_num_of_pos, high_num_of_pos + 1):
-    all_pos_same_number = [str(try_number) * digit for try_number in range(1, 10)]
+    all_pos_same_number = [str(try_number) * digit for try_number in range(0, 10)]
     for cur_number in all_pos_same_number:
         base_value = int(cur_number[0])
         for cur_pos in range(digit):
             for try_num in range(0, 10):
-                if base_value == try_num:
+                if (cur_pos == 0 and try_num == 0) or base_value == try_num:
                     continue
                 suspect_number = int(replace_char_at_index(cur_number, cur_pos, str(try_num)))
                 if low <= suspect_number <= high:
-                    suspect_numbers.append(suspect_number)
-
+                    suspect_numbers.add(suspect_number)
+                
 print(len(suspect_numbers))
