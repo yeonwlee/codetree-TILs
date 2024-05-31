2b133f45a1bb8670a8823945a33f2693b5d763f9
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-01 03:14:43 +0900

    Title: 색종이 1, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240601/색종이 1/color-paper-1.py
@@ -0,0 +1,20 @@
+num_of_papers = int(input())
+
+# 가로, 세로의 크기가 각각 10인 정사각형 모양의 파란색 색종이
+# 왼쪽 아래의 x 좌표와 y 좌표
+papers_pos_info = [
+    tuple(map(int, input().split()))
+    for _ in range(num_of_papers)
+]
+
+grid = [
+    [0] * 91
+    for _ in range(91)
+]
+
+for under_left_x, under_left_y in papers_pos_info:
+    for x in range(under_left_x, under_left_x + 10):
+        for y in range(under_left_y, under_left_y + 10):
+            grid[x][y] = 1
+
+print(sum(1 for row in grid for col in row if col == 1))
