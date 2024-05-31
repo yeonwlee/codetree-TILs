c01f2dcf29b90031616bd0971b1a46ef8c72a38a
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-01 02:43:03 +0900

    Title: 부분 문자열의 존재 유무, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240601/부분 문자열의 존재 유무/presence-of-substring.py
+++ 240601/부분 문자열의 존재 유무/presence-of-substring.py
@@ -1,3 +1,3 @@
 source_str = input() # 알파벳 대소문자로만. 공백없음
 
-print('YES' if source_str.find('ee') else 'NO', 'YES' if source_str.find('ea') else 'NO')
+print('YES' if source_str.find('ee') != -1 else 'NO', 'YES' if source_str.find('ea') != -1 else 'NO')
