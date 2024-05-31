ed8c9d3fe5a388d539a8a460a233bd8c876fa48d
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-01 02:15:26 +0900

    Title: 3개의 선 2, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240601/3개의 선 2/three-lines-2.py
+++ 240601/3개의 선 2/three-lines-2.py
@@ -17,12 +17,14 @@ set_x_coord = set(coords_dict['x'])
 set_y_coord = set(coords_dict['y'])
 
 is_possible = False
-for num_of_y_line in range(min(len(set_x_coord)+1, 4)):
-    x_coord_combies = list(combinations(set_x_coord, num_of_y_line))
+for num_of_y_line in range(min(len(set_x_coord)+1, 4)): # 최소 set의 개수 만큼, 최대 3개
+    x_coord_combies = list(combinations(set_x_coord, num_of_y_line)) # 해당 선분 수 만큼의 조합 만들기
     for cur_y_lines in x_coord_combies:
-        remain_y_coord = {coords_dict['y'][index] for index, number in enumerate(coords_dict['x']) if number not in cur_y_lines}
-        if 3 - num_of_y_line - len(remain_y_coord) >= 0:
-            is_possible = True
+        # 해당 선분에서 덮어씌워질 점 외에, y좌표를 set으로 모음
+        y_coord = {coords_dict['y'][index] for index, number in enumerate(coords_dict['x']) if number not in cur_y_lines}
+        # 3개 선분 - x 좌표를 지우는 데 사용했던 선분의 수 - 남은 y 좌표를 지우는 데 필요한 선분의 수가 양수면
+        if 3 - num_of_y_line - len(y_coord) >= 0:
+            is_possible = True # 선분 3개로 지우기 가능
             break
 if is_possible:
     print(1)
