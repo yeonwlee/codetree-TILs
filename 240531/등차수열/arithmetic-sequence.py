062ab4e3dcb00d19f68b4c25016792663fcfcf72
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-31 21:51:38 +0900

    Title: 등차수열, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240531/등차수열/arithmetic-sequence.py
@@ -0,0 +1,21 @@
+num_of_numbers = int(input())
+numbers = list(map(int, input().split()))
+
+# 1 <= 주어지는 숫자 <= 100 
+# 1, 100 이 주어졌을 때 최대 차 99
+
+max_num = max(numbers)
+min_num = min(numbers)
+max_count_of_arithmetic_sequence = 0
+
+for number in range(-max_num, max_num + max_num - min_num):
+    cur_count_of_arithmetic_sequence = 0
+    for a_index in range(len(numbers)-1):
+        first_num = numbers[a_index]
+        for b_index in range(a_index + 1, len(numbers)):
+            third_num = numbers[b_index]
+            if abs(first_num - number) == abs(b_index - number):
+                cur_count_of_arithmetic_sequence += 1
+    max_count_of_arithmetic_sequence = max(max_count_of_arithmetic_sequence, cur_count_of_arithmetic_sequence)
+
+print(max_count_of_arithmetic_sequence)
