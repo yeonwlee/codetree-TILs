4032a4cfee63108d5f5c87c09b132f53816a129d
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-05-31 22:50:42 +0900

    Title: 빙산의 일각 2, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

--- 240531/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240531/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -6,7 +6,9 @@ ice_height_info = [
 ]
 
 max_num_of_ice = 0
+# 해수면 레벨
 for sea_lavel in range(1, max(ice_height_info) - 1):
+    #해수면보다 높으면 '1', 낮으면 '0'으로 변경후 str 화
     ice = ''.join(['1' if ice_height > sea_lavel else '0' for ice_height in ice_height_info])
     max_num_of_ice = max(max_num_of_ice, sum(1 for cur_ice_piece in ice.split('0') if cur_ice_piece != ''))
 
