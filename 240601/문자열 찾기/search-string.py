ea29f0bcb14c459a9dbc9e3fa08187a791c5d2a2
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-01 03:41:11 +0900

    Title: 문자열 찾기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240601/문자열 찾기/search-string.py
+++ 240601/문자열 찾기/search-string.py
@@ -1,2 +1,14 @@
 source = input()
-print(source.count('KOI'), source.count('IOI'))
+# print(source.count('KOI'), source.count('IOI'))
+
+count_of_koi = 0
+count_of_ioi = 0
+
+for index in range(len(source) - 3):
+    cur_word = source[index: index + 3]
+    if cur_word == 'KOI':
+        count_of_koi += 1
+    elif cur_word == 'IOI':
+        count_of_ioi += 1
+
+print(count_of_koi, count_of_ioi)
