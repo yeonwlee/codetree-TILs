14dee5222b01ac3f2362457bcdf557ab0ba00abe
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-02 22:13:36 +0900

    Title: 좌표평면 위의 균형 2, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240602/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 240602/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -4,17 +4,9 @@ points = [
     for _ in range(num_of_points)
 ]
 
-min_x, min_y = float('inf'), float('inf')
-max_x, max_y = float('-inf'), float('-inf')
-for x, y in points:
-    min_x = min(min_x, x)
-    max_x = max(max_x, x)
-    min_y = min(min_y, y)
-    max_y = max(max_y, y)
-
 min_largest_number_of_points = float('inf')
-for x in range(min_x, max_x + 1):
-    for y in range(min_y, max_y + 1):
+for x in range(1, 101):
+    for y in range(1, 101):
         area_points = [0, 0, 0, 0]
         for region_x, region_y in points:
             if region_x < x and region_y > y:
