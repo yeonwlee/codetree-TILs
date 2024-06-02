e8af7bbfc9fee6d962cb734f86a828719a00ea30
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-02 23:16:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240602/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,24 @@
+from itertools import zip_longest
+
+tictactoe = [
+    list(input())
+    for _ in range(3)
+]
+
+# 가로, 세로, 대각선 등 승리하는 케이스를 모두 모으기
+# 각 케이스 중 두 개의 수로 만들어진 경우 세기
+victory_cases = set()
+
+# 가로
+for row in tictactoe:
+    victory_cases.add(tuple(row))
+
+# 세로    
+for col in range(3):
+    victory_cases.add(tuple([row[col] for row in tictactoe]))
+
+# 대각선
+victory_cases.add((tictactoe[0][0], tictactoe[1][1], tictactoe[2][2]))
+victory_cases.add((tictactoe[0][2], tictactoe[1][1], tictactoe[2][0]))
+
+print(sum(1 for cur_case in victory_cases if len(set(cur_case)) == 2))
