aca1398473074af68d18b631876bd43ccad372f1
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-02 23:55:48 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240602/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+source_len = int(input().rstrip())
+source = input().rstrip()
+
+# 연속 부분 문자열로 두 번 이상 나타나지 않는 문자열의 최소 길이를 출력하는 프로그램을 작성하세요.
+for length in range(1, source_len // 2 + 2): # 두 번 이상 나타날 수 있는 문자열 길이까지 확인하면 됨
+    for check_index in range(source_len):
+        if check_index + length > source_len: # 모두 찾을 때 까지 해당 문자열을 2개 이상 못 찾았으면
+            print(length) # 문자열의 길이 출력
+            break
+        elif source.count(str(source[check_index:check_index + length])) >= 2: # 소스 문자열에서 해당 길이만큼 자른(부분문자열)의 갯수가 2개 이상이면
+            break
