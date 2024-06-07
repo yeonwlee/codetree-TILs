7b1c6e3cce99f948e2c841895428dc868eb4730a
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-07 19:15:28 +0900

    Title: 독서실의 거리두기 4, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240607/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 240607/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,26 +1,25 @@
 def get_nearest_distance()-> int :
-    min_connective_empty_seat: int = float('inf')
+    min_distance: int = float('inf')
     left: int = 0
     for right in range(num_of_seat):
-        if any(True if seat == '1' else False for seat in seat_info[left:right + 1]):
-            left += 1
-        else:
-            min_connective_empty_seat = min(min_connective_empty_seat, right - left + 1) # 연속된 빈 좌석의 수
+        if seat_info[right] == '1' and right != 0:
+            min_distance  = min(min_distance, right - left)
+            left = right
+    if left != right:
+        min_distance = min(min_distance, num_of_seat - left)
 
-    return min_connective_empty_seat + 1 # 1001 을 예로 들면, 1과 1 사이의 거리는 3임(연속된 빈좌석 수 + 1).
+    return min_distance 
 
 
 # 두 명을 더 집어넣었을 때 가장 가까운 두 사람 간의 거리를 최대로 하고 싶다
 num_of_seat = int(input())
 seat_info = list(input())
-empty_seat_info = {index for index, value in enumerate(seat_info) if value == '0'}
+empty_seat_info = [index for index, value in enumerate(seat_info) if value == '0']
 
 max_distance = 0
-for first in empty_seat_info:
+for first_index, first in enumerate(empty_seat_info):
     seat_info[first] = '1'
-    for second in empty_seat_info:
-        if first == second:
-            continue
+    for second in empty_seat_info[first_index + 1:]:
         seat_info[second] = '1'
         max_distance = max(max_distance, get_nearest_distance())
         seat_info[second] = '0'
