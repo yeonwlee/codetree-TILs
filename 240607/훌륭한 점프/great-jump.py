e06193c5e5b38b818e4145241e0e5e72e7ec6b84
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-07 15:59:23 +0900

    Title: 훌륭한 점프, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240607/훌륭한 점프/great-jump.py
+++ 240607/훌륭한 점프/great-jump.py
@@ -5,11 +5,12 @@ stones = list(map(int, input().split()))
 cur_stone_value = stones[0]
 for num in range(cur_stone_value, 101): # 조건 범위, 최대 값 가정
     cur_pos: int = 0
-    # 해당 값을 최대값으로 했을 때, 밟을 수 있는 돌중 cur_pos 이후의 돌을 꺼냄.
-    while (distance_of_can_jump_stone:=next((jump_distance for jump_distance, value in enumerate(stones) if value <= num and cur_pos < jump_distance), None)):
-        if abs(cur_pos - distance_of_can_jump_stone) <= max_jump_distance: # 뛸 수 있음
-            cur_pos += abs(cur_pos - distance_of_can_jump_stone)
-        else: # 뛸 수 없음
+    # 해당 값을 최대값으로 했을 때, 밟을 수 있는 돌 밟고 건너가기
+    for index in range(cur_pos + 1, num_of_stone):
+        if abs(cur_pos - index) <= max_jump_distance:
+            if stones[index] <= num:
+                cur_pos += abs(cur_pos - index)
+        else:
             break
     if cur_pos == num_of_stone - 1: # 마지막 돌에 위치함
         print(num)
