bafdc3c51f9057cf304f7f8f90a384e2e469e0d9
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-07 15:29:20 +0900

    Title: 훌륭한 점프, Time: 258ms, Memory: 25MB, Status: Passed - CodeTree

--- 240607/훌륭한 점프/great-jump.py
+++ 240607/훌륭한 점프/great-jump.py
@@ -2,8 +2,8 @@ num_of_stone, max_jump_distance = map(int, input().split())
 
 stones = list(map(int, input().split()))
 
-
-for num in range(1, 101): # 조건 범위, 최대 값 가정
+cur_stone_value = stones[0]
+for num in range(cur_stone_value, 101): # 조건 범위, 최대 값 가정
     cur_pos: int = 0
     while (distance_of_can_jump_stone:=next((jump_distance for jump_distance, value in enumerate(stones) if value <= num and cur_pos < jump_distance), None)):
         if abs(cur_pos - distance_of_can_jump_stone) <= max_jump_distance: # 범위 내로 뛸 수 있는 곳이 있음
