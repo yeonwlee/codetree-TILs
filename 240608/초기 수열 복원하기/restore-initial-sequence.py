3642d2d3191f45b183ff956f71f3ddd98b900338
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-08 19:13:21 +0900

    Title: 초기 수열 복원하기, Time: 1383ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240608/초기 수열 복원하기/restore-initial-sequence.py
+++ 240608/초기 수열 복원하기/restore-initial-sequence.py
@@ -4,7 +4,6 @@ max_num: int = int(input())
 sum_arr = list(map(int, input().split()))
 
 # 모든 조합 만들기
-permutations_source = sorted(list(permutations(range(1, max_num + 1))))
-for cur_combi in permutations_source:
+for cur_combi in sorted(permutations(range(1, max_num + 1))):
     if all(cur_sum == cur_combi[index] + cur_combi[index + 1] for index, cur_sum in enumerate(sum_arr)):
         print(' '.join(map(str, cur_combi)))
