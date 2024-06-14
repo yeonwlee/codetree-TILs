4dbbad8ca78a9a2e6e123f1a785cdaa3f87c054e
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-14 20:09:13 +0900

    Title: 진법 변환 7, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240614/진법 변환 7/base-conversion-7.py
+++ 240614/진법 변환 7/base-conversion-7.py
@@ -1,17 +1,28 @@
 import math
 
-def get_decimal_bin(decimal:float) -> int:
- 
-    return
+# # 풀이 1: 다소 장황
+# number = float(input())
+# length_of_decimal = len(str(number).split('.')[-1])
+# decimal_answer = []
+# decimal = number % 1
+# while len(decimal_answer) != 4:
+#     decimal = math.trunc(decimal * pow(10, length_of_decimal)) / pow(10, length_of_decimal)
+#     num_pos, decimal = divmod(decimal * 2, 1)
+#     decimal_answer.append(str(int(num_pos)))
 
+# answer = bin(math.trunc(number))[2:] + '.' + ''.join(decimal_answer)
+# print(answer)
+
+# 풀이2: 뺄셈을 활용해보자
 number = float(input())
-length_of_decimal = len(str(number).split('.')[-1])
-decimal_answer = []
-decimal = number % 1
-while len(decimal_answer) != 4:
-    decimal = math.trunc(decimal * pow(10, length_of_decimal)) / pow(10, length_of_decimal)
-    num_pos, decimal = divmod(decimal * 2, 1)
-    decimal_answer.append(str(int(num_pos)))
+num_pos = math.trunc(number)
+answer = bin(num_pos)[2:] # 전체를 모을 문자열
 
-answer = bin(math.trunc(number))[2:] + '.' + ''.join(decimal_answer)
-print(answer)
+decimal_answer = [] # 소수부
+while len(decimal_answer) != 4:
+    decimal = number - num_pos
+    number = decimal * 2    
+    num_pos = math.trunc(number)
+    decimal_answer.append(str(num_pos))
+
+print(answer + '.' + ''.join(decimal_answer))
