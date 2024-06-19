cb6d9621f5dc4493f8ac49dfb0d614b358d053a2
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-06-20 08:31:51 +0900

    Title: 4가지 관계연산자, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240620/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,21 @@
+num1, num2 = map(int, input().split())
+
+if num1 >= num2:
+    print(1)
+else:
+    print(0)
+
+if num1 > num2:
+    print(1)
+else:
+    print(0)
+
+if num2 >= num1:
+    print(1)
+else:
+    print(0)
+
+if num2 > num1:
+    print(1)
+else:
+    print(0)
