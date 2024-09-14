22a208cb7dd9cedbc0b1d9a955c6c675c73d1536
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-09-14 19:03:51 +0900

    Title: 계단 오르기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240914/계단 오르기/climbing-stairs.py
+++ 240914/계단 오르기/climbing-stairs.py
@@ -8,7 +8,7 @@ def count_case(cur_num:int, memo:dict[int]={}) -> int:
     elif cur_num == 0:
         return 1
     memo[cur_num] = count_case(cur_num - 2) + count_case(cur_num - 3)
-    return memo[cur_num]
+    return memo[cur_num] // 10007
 
 
 print(count_case(num))
