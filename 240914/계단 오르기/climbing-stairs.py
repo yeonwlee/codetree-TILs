05ce3badfd6ff1623dd202aa9f647dfcf5dd4ffa
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-09-14 19:02:53 +0900

    Title: 계단 오르기, Time: 5046ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240914/계단 오르기/climbing-stairs.py
+++ 240914/계단 오르기/climbing-stairs.py
@@ -1,12 +1,12 @@
 num = int(input())
 
-def count_case(cur_num:int) -> int:
+def count_case(cur_num:int, memo:dict[int]={}) -> int:
     if cur_num < 0:
         return 0
     elif cur_num == 0:
         return 1
-
-    return count_case(cur_num - 2) + count_case(cur_num - 3)
+    memo[cur_num] = count_case(cur_num - 2) + count_case(cur_num - 3)
+    return memo[cur_num]
 
 
 print(count_case(num))
