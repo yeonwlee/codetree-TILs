919aa8bd4744644df3177f954ab6aef9b52c3718
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-09-14 18:59:33 +0900

    Title: 계단 오르기, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240914/계단 오르기/climbing-stairs.py
+++ 240914/계단 오르기/climbing-stairs.py
@@ -1,13 +1,12 @@
 num = int(input())
 
 def count_case(cur_num:int) -> int:
-    if cur_num == num:
+    if cur_num < 0:
+        return 0
+    elif cur_num == num:
         return 1
 
-    count = 0
-    for step in [2, 3]:
-        count += count_case(cur_num - step)
-    return count
+    return count_case(cur_num - 2) + count_case(cur_num - 3)
 
 
 print(count_case(num))
